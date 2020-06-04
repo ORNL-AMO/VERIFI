f2fcbb44be55e74ef7066623f813256281fa0ab5
Cranfill <cranfillkd@ornl.gov>
2020-06-04 08:37:25 -0400

    Footer Updates

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { version } from '../../../../package.json';
+import { AccountService } from "../../account/account/account.service";
+import { AccountdbService } from "../../indexedDB/account-db.service";
+import { FacilitydbService } from "../../indexedDB/facility-db-service";
 
 @Component({
   selector: 'app-footer',
@@ -6,10 +10,66 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+  accountid: number;
+  date = new Date();
+  version: string = version;
+  accountCount: number = 0;
+  facilityCount: number = 0;
+  facilityCountTotal: number = 0;
 
-  constructor() { }
+  constructor(
+    public accountService: AccountService,
+    public accountdbService: AccountdbService,
+    public facilitydbService: FacilitydbService,
+  ) { 
+
+  }
 
   ngOnInit() {
+    // Subscribe to account ID
+    this.accountService.getValue().subscribe((value) => {
+      this.accountid = value;
+      this.countFacilities();
+    });
+
+    this.countAccounts();
+    this.countAllFacilities();
+  }
+
+  countAccounts() {
+    // Count all accounts
+    this.accountdbService.count().then(
+      data => {
+        this.accountCount = data;
+      },
+      error => {
+          console.log(error);
+      }
+    );
+  }
+
+  countFacilities() {
+    // Count current facilities
+    this.facilitydbService.getAllByIndex(this.accountid).then(
+      data => {
+        this.facilityCount = data.length;
+      },
+      error => {
+          console.log(error);
+      }
+    );
+  }
+
+  countAllFacilities() {
+    // Count all facilities
+    this.facilitydbService.count().then(
+      data => {
+        this.facilityCountTotal = data;
+      },
+      error => {
+          console.log(error);
+      }
+    );
   }
 
 }
