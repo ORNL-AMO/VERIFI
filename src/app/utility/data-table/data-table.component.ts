eb8d323b9202593d37625850e65990e9941c676f
Cranfill <cranfillkd@ornl.gov>
2020-07-17 12:59:05 -0400

    Meter Import/Export

--- src/app/utility/data-table/data-table.component.ts
+++ src/app/utility/data-table/data-table.component.ts
@@ -67,8 +67,10 @@ export class DataTableComponent implements OnInit {
         this.utilityMeterDatadbService.getAllByIndex(this.meterList[i]['id']).then(
           data => {
             // push to meterlist object
-            this.meterList[i]['data'] = data;
+            this.meterList[i]['data'] = data.sort(this.sortByDate);;
+            //this.meterList[i]['data'].sort(this.sortByDate);
             this.meterList[i]['calendarization'] = this.calendarization(data, 'Elec');
+            console.log(this.meterList);
           },
           error => {
               console.log(error);
@@ -80,7 +82,8 @@ export class DataTableComponent implements OnInit {
         this.naturalGasdbService.getAllByIndex(this.meterList[i]['id']).then(
           data => {
             // push to meterlist object
-            this.meterList[i]['data'] = data;
+            this.meterList[i]['data'] = data.sort(this.sortByDate);;
+            //this.meterList[i]['data'].sort(this.sortByDate);
             this.meterList[i]['calendarization'] = this.calendarization(data, 'NatGas');
           },
           error => {
@@ -92,18 +95,21 @@ export class DataTableComponent implements OnInit {
     }
   }
 
+  sortByDate(a, b) {
+    return new Date(a.readDate).getTime() - new Date(b.readDate).getTime();
+  }
+
   calendarization (data, type) {
-    console.log(data);
     this.dataTable = [];
 
     for(let i=1; i < data.length -1; i++) {
       
       let billDate = new Date(data[i]['readDate']);
-      const monthName = billDate.toLocaleString('default', { month: 'long' });
       let billDateNext = new Date(data[i]['readDate']);;
       let billDatePrev = new Date(data[i]['readDate']);;
-      //console.log("--------------");
-      //console.log(monthName);
+
+      const monthName = billDate.toLocaleString('default', { month: 'long' });
+      const year = billDate.getFullYear();
 
       // End of billing cycle (meter read date)
       // prevent errors on 1st and last entry
@@ -113,12 +119,10 @@ export class DataTableComponent implements OnInit {
       } else if ( i == data.length - 1) {
         billDateNext.setMonth(billDateNext.getMonth() + 1);
         billDatePrev = new Date(data[i-1]['readDate']);
-        //console.log("BillNext: "+billDateNext);
       } else {
         billDateNext = new Date(data[i+1]['readDate']);
         billDatePrev = new Date(data[i-1]['readDate']);
       }
-
 
       // First of month (1)
       let firstDay = new Date(billDate); firstDay.setDate(1);
@@ -126,74 +130,74 @@ export class DataTableComponent implements OnInit {
 
       // Days in billing cycle (next meter date - prev meter date)
       const difference = this.dateDiffInDays(billDatePrev, billDate);
-      const differenceNext = this.dateDiffInDays(billDate, billDateNext); //*
-      //console.log("diff: " + difference);
+      const differenceNext = this.dateDiffInDays(billDate, billDateNext);
 
-      // Days before and after
-      const daysBefore = this.dateDiffInDays(firstDay, billDate) + 1;
-      const daysAfter = this.dateDiffInDays(billDate, firstDayNext) - 1;
-      //console.log("days before: "+daysBefore);
-      //console.log("days after: "+daysAfter);
-      //console.log("before: " + volPerDay * daysBefore);
-      //console.log("after: " + volPerDayNext * daysAfter);
+      // Prevents skipped months from displaying
+      if (difference > 28 && difference < 40) {
+        // Days before and after
+        const daysBefore = this.dateDiffInDays(firstDay, billDate) + 1;
+        const daysAfter = this.dateDiffInDays(billDate, firstDayNext) - 1;
 
-      // Billed cost & volume
-      let vol = 0;
-      let volNext = 0;
-      let cost = 0;
-      let costNext = 0;
-      
-      if ( i == data.length - 1) { // If last entry
-        if (type == 'Elec') {
-          vol = data[i]['totalKwh'];
-        }
-        if (type == 'NatGas') {
-          vol = data[i]['totalVolume'];
-        }
-        cost = data[i]['totalCost'];
+        // Billed cost & volume
+        let vol = 0;
+        let volNext = 0;
+        let cost = 0;
+        let costNext = 0;
         
+        if ( i == data.length - 1) { // If last entry
+          if (type == 'Elec') {
+            vol = data[i]['totalKwh'];
+          }
+          if (type == 'NatGas') {
+            vol = data[i]['totalVolume'];
+          }
+          cost = data[i]['totalCost'];
+          
+        } else {
+          if (type == 'Elec') {
+            vol = data[i]['totalKwh'];
+            volNext = data[i+1]['totalKwh'];
+          }
+          if (type == 'NatGas') {
+            vol = data[i]['totalVolume'];
+            volNext = data[i+1]['totalVolume'];
+          }
+          cost = data[i]['totalCost'];
+          costNext = data[i+1]['totalCost'];
+        }
+
+        // Volume per day (billed volume / days in cycle)
+        const volPerDay = vol / difference;
+        const volPerDayNext = volNext / differenceNext;
+        let monthlyVol = volPerDay * daysBefore + volPerDayNext * daysAfter;
+        if (isNaN(monthlyVol)) {
+          monthlyVol = 0;
+        }
+
+        // Cost per day (billed volume / days in cycle)
+        const costPerDay = cost / difference;
+        const costPerDayNext = costNext / differenceNext;
+        let monthlyCost = costPerDay * daysBefore + costPerDayNext * daysAfter;
+        if (isNaN(monthlyCost)) {
+          monthlyCost = 0;
+        }
+
+        this.dataTable.push({
+          year: year,
+          month: monthName,
+          monthKwh: monthlyVol.toFixed(2),
+          monthCost: monthlyCost.toFixed(2)
+        });
+
       } else {
-        if (type == 'Elec') {
-          vol = data[i]['totalKwh'];
-          volNext = data[i+1]['totalKwh'];
-        }
-        if (type == 'NatGas') {
-          vol = data[i]['totalVolume'];
-          volNext = data[i+1]['totalVolume'];
-        }
-        cost = data[i]['totalCost'];
-        costNext = data[i+1]['totalCost'];
+        // Show month, but don't calculate.
+        this.dataTable.push({
+          year: year,
+          month: monthName,
+          monthKwh: 'NA',
+          monthCost: 'NA'
+        });
       }
-      
-      
-
-      // if days after = 0
-      // Volume per day (billed volume / days in cycle)
-      const volPerDay = vol / difference;
-      const volPerDayNext = volNext / differenceNext;
-      let monthlyVol = volPerDay * daysBefore + volPerDayNext * daysAfter;
-      if (isNaN(monthlyVol)) {
-        monthlyVol = 0;
-      }
-      //console.log("vol:  "+ vol);
-      //console.log("vol/day:  "+ volPerDay);
-
-
-      // Cost per day (billed volume / days in cycle)
-      const costPerDay = cost / difference;
-      const costPerDayNext = costNext / differenceNext;
-      let monthlyCost = costPerDay * daysBefore + costPerDayNext * daysAfter;
-      if (isNaN(monthlyCost)) {
-        monthlyCost = 0;
-      }
-
-      this.dataTable.push({
-        month: monthName,
-        monthKwh: monthlyVol.toFixed(2),
-        monthDemand: 0,
-        monthCost: monthlyCost.toFixed(2)
-      });
-      
     }
     return this.dataTable;
   }
