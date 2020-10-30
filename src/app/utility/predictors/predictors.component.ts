4307917879731e05bb630e6337c639654487baf3
Cranfill <cranfillkd@ornl.gov>
2020-10-30 08:57:11 -0400

    Starting predictors

--- src/app/utility/predictors/predictors.component.ts
+++ src/app/utility/predictors/predictors.component.ts
@@ -6,10 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./predictors.component.css']
 })
 export class PredictorsComponent implements OnInit {
+  months: any = [
+    'Jan',
+    'Feb',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'Aug',
+    'Sept',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  predictor: any = ["Production 1","Production 2","Production 3"]
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
