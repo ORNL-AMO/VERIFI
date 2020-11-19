24f82491a69809e2e49bfe8943c204770187f451
Cranfill <cranfillkd@ornl.gov>
2020-11-19 14:50:51 -0500

    Adding predictor db and predictor component table

--- src/app/utility/predictors/predictors.component.ts
+++ src/app/utility/predictors/predictors.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 import { PredictorsService } from 'src/app/utility/predictors/predictors.service';
 
 @Component({
@@ -7,40 +8,94 @@ import { PredictorsService } from 'src/app/utility/predictors/predictors.service
   styleUrls: ['./predictors.component.css']
 })
 export class PredictorsComponent implements OnInit {
-  months: any = ['Jan','Feb','March','April','May','June','July','Aug','Sept','Oct','Nov','Dec'];
-  popup: boolean = false;
-  dataTable = [];
-  dates = [];
+  editPredictorWindow: boolean = false;
+  editPredictorDataWindow: boolean = false;
+  predictorMenu: string;
 
-  predictors: any = [];
-  predictorHeaders: any = [];
+  dataTable: Array<object>;
+  dates: Array<string>;
 
-  page = 1;
-  itemsPerPage = 12;
+  predictors: Array<object>;
+  predictorHeaders: Array<string> = [];
+  predictorRow: Array<object> = [];
+
+  page: number = 1;
+  itemsPerPage: number = 12;
   pageSize: number;
+
+  predictorHeaderForm:FormGroup;
+  predictorRowForm:FormGroup;
+
+  /* Temp vars for delete confirm */
+  deleteFilter: string;
+  deleteValue: string;
+  popup: boolean = false;
 
   constructor(private predictorsService: PredictorsService) { }
 
   ngOnInit(): void {
+    this.getPredictors();
+  }
+
+  getPredictors(): void {
     // Observe the Predictors
     this.predictorsService.getPredictors().subscribe((value) => {
-      
+          
       // Predictor values
       this.predictors = value;
-      console.log("predictors");
-      console.log(this.predictors);
-
       
       // Headers for table
-      this.predictorHeaders = [...new Set(value.map(item => item.name))];
+      this.predictorHeaders = [...new Set(value.map(item => item['name']) as unknown as string)];
 
       // Data for table
       this.formatPredictorTable();
 
+      // Set to last page
+      const lastPage = Math.ceil(this.dataTable.length/this.itemsPerPage);
+      this.page = lastPage;
+      this.onPageChange(lastPage);
+
+      // Edit form for predictor headers
+      this.setDynamicHeaderForm();
+      this.setDynamicRowForm();
     });
   }
+
+  documentClick (): void {
+    // Close menus when user clicks outside the dropdown
+    this.predictorMenu = null;
+  }
   
-  formatPredictorTable() {
+  predictorToggleMenu (index): void {
+    if (this.predictorMenu === index) {
+      this.predictorMenu = null;
+    } else {
+      this.predictorMenu = index;
+    }
+  }
+
+  setDynamicHeaderForm(): void {
+    let group={};
+    this.predictorHeaders.forEach(input_template=>{
+      group[input_template]=new FormControl(input_template);  
+    })
+    this.predictorHeaderForm = new FormGroup(group);
+  }
+
+  setDynamicRowForm(): void {
+    let group={};
+    // add date
+    group['date']=new FormControl('');
+
+    // add each predictor row
+    this.predictorRow.forEach(input_template=>{
+      group[input_template['name']]=new FormControl(input_template['amount']);  
+    });
+    
+    this.predictorRowForm = new FormGroup(group);
+  }
+  
+  formatPredictorTable(): void {
       // Create new object based on month/date
       // Add corresponding amounts as array inside date obj
       this.dataTable = [];
@@ -50,7 +105,7 @@ export class PredictorsComponent implements OnInit {
        // Store new results
       let data = tempPredictors.reduce((result, item) => {
         const group = (result[item.date] || []);
-        group.push(item.name); // amounts
+        group.push({id: item.id, amount: item.amount}); // amounts
         result[item.date] = group;
         return result;
       }, {});
@@ -61,66 +116,193 @@ export class PredictorsComponent implements OnInit {
         this.dataTable[i]['date'] = Object.entries(data)[i][0];
         this.dataTable[i]['amount'] = Object.entries(data)[i][1];
      }
-      console.log("data");
-      console.log(data);
-      console.log(this.dates);
-      console.log(this.dataTable);
+
+     this.dates.sort(this.sortByItem);
+     this.dataTable.sort(this.sortByDate);
   }
 
-  predictorAdd() {
-    /*let backlog = [0];
+  sortByItem(a, b) {
+    return new Date(a).getTime() - new Date(b).getTime();
+  }
+  sortByDate(a, b) {
+    return new Date(a.date).getTime() - new Date(b.date).getTime();
+  }
 
-    // count how many predictor rows
-    if(this.predictorHeaders.length != 0) {
-      const firstPredictor = this.predictorHeaders[0]
-      backlog = this.predictors.filter(function(obj) {
-        return obj.name == firstPredictor;
-      });
-    }*/
+  predictorAdd(): void {
+    /* this.dates is an array of the different dates in the predictor table.
+    When a new predictor is added, this function makes sure all of the 
+    current dates are added to the new predictor and set to 0. */
 
+    // If this is the first predictor, set a default date.
     if (this.dates.length === 0) {
-      this.dates.push('Jan 2020');
+      this.dates.push('2020-01');
     }
-    this.predictorsService.addPredictor(this.dates);
-  }
 
-  predictorAddRow() {
-    this.predictorsService.addPredictorRow(this.predictorHeaders,'March 2020');
-  }
-
-  predictorEdit(id) {
-    this.popup = !this.popup;
-    //this.predictorMenuOpen = null;
-    //this.predictorForm.setValue(this.meterpredictors.find(obj => obj.id == id)); // Set form values to current selected meter
-    //this.predictorForm.controls.dateModified.setValue(this.today);
-  }
-
-/*
-  predictorDelete(id) {
-    this.predictorMenuOpen = null;
-
-    // Check if all meters have been removed from the predictor
-    if(this.checkpredictorData(id)) {
-      this.utilityMeterpredictordbService.deleteIndex(id);
-      // Refresh list of predictors
-      // I splice the array vs refreshing the list because its faster for the user.
-      const index = this.meterpredictors.map(e => e.id).indexOf(id);
-      this.meterpredictors.splice(index, 1); // remove it
-    }
-  }
-
-  predictorSave() {
-    this.popup = !this.popup;
-    this.predictorMenuOpen = null;
-    
-    // Check if name is unique before updating
-    if (this.checkpredictorName()) {
-      this.utilityMeterpredictordbService.update(this.predictorForm.value);
+    if (this.predictorHeaders.length < 10) {
+      this.predictorsService.addPredictor(this.dates, this.predictorHeaders.length);
+    } else {
+      alert("You're max of 10 predictor variables has been reached.");
     }
     
-    this.predictorLoadList(); // Refresh list of predictors
   }
-*/
+
+  predictorAddRow(): void {
+    /* this.predictorHeaders is an array of the different predictors in the predictor table.
+    When you add a new row, it adds a new date per predictor. */
+    let nextYear: string | number;
+    let nextMonth: string | number;
+
+    // get last date
+    const lastDateIndex = this.dates.length - 1;
+    const lastDate = this.dates[lastDateIndex];
+
+    const lastYear = lastDate.split("-")[0];
+    const lastMonth = lastDate.split("-")[1];
+
+    // get new date
+    nextYear = lastYear;
+    nextMonth = +lastMonth + 1;
+
+    // if last month is Dec, increase year and reset to Jan
+    if (nextMonth === 13) {
+
+      // increase year
+       nextYear = (+lastYear + 1).toString(); 
+
+      // choose next month
+      nextMonth = 1;
+    }
+    
+    if (nextMonth.toString().length === 1) {
+      nextMonth = "0"+nextMonth;
+    }
+    const nextDate = nextYear + "-" + nextMonth;
+
+    this.predictorsService.addPredictorRow(this.predictorHeaders,nextDate);
+  }
+
+  predictorEdit(): void {
+    this.editPredictorWindow = !this.editPredictorWindow;
+    this.predictorMenu = null;
+  }
+
+  predictorDataEdit(date): void {
+    this.editPredictorDataWindow = !this.editPredictorDataWindow;
+    this.predictorMenu = null;
+    
+    
+    // filter all predictors with this date
+    this.predictorRow = this.predictors.filter(function(obj) {
+      return obj['date'] == date;
+    });
+
+    // refresh form
+    this.setDynamicRowForm();
+
+    // set date equal to selected row
+    this.predictorRowForm.patchValue({date: this.predictorRow[0]['date']});
+
+  }
+
+  predictorSave(): void {
+    this.editPredictorWindow = !this.editPredictorWindow;
+    const formControls = Object.keys(this.predictorHeaderForm.controls);
+
+    // for each object
+    for (let i=0; i < formControls.length; i++) {
+      const control = formControls[i]; // Get old value by the key name
+      const controlValue = (this.predictorHeaderForm.get(control)).value; // Get new input value
+
+      // Quick refresh list of predictor headers
+      const index = this.predictorHeaders.map(e => e).indexOf(control);
+      this.predictorHeaders.splice(index, 1, controlValue); // remove it
+
+      // get everything with that form name
+      this.predictorsService.getAllByName(control).then(
+        data => {
+
+          // update everything with form name
+          for (let item of data) {
+            item['name'] = controlValue;
+
+            // save the values
+            this.predictorsService.updatePredictor([item]);
+          }
+          
+        },
+        error => {
+            console.log(error);
+        }
+      );
+      
+    }
+
+    // Edit predictor headers for form
+    //this.setDynamicHeaderForm();
+  }
+
+  predictorDataSave(): void {
+    this.editPredictorDataWindow = !this.editPredictorDataWindow;
+    const formDate = this.predictorRowForm.controls['date'].value;
+    const formControls = Object.keys(this.predictorRowForm.controls);
+    const formValues = Object.values(this.predictorRowForm.value);
+
+    // update date
+    for (let i=0; i<this.predictorRow.length; i++) {
+      this.predictorRow[i]['date'] = formDate;
+    }
+    
+    // update predictor rows
+    for (let i=1; i<formControls.length; i++) {
+      // find row 
+      const index = this.predictorRow.map(function(e) { return e['name']; }).indexOf(formControls[i]);
+
+      // replace value
+      this.predictorRow[index]['amount'] = formValues[i];
+    }
+
+    // update predictor db
+    this.predictorsService.updatePredictor(this.predictorRow);
+
+    // refresh table
+    this.getPredictors();
+  }
+
+  checkDate() {
+    // check if date already exists
+    const formDate = this.predictorRowForm.controls['date'].value;
+    const check = this.predictors.map(function(e) { return e['date']; }).indexOf(formDate);
+    if (check != -1) {
+      alert("This date already exists.");
+      this.predictorRowForm.controls['date'].setValue(this.predictorRow[0]['date']); //revert date
+    }
+  }
+
+  deleteConfirm (filterType, name): void {
+    this.popup = true;
+    this.deleteFilter = filterType;
+    this.deleteValue = name;
+  }
+
+  deletePredictors (filterType, name): void {
+    this.predictorMenu = null;
+
+    const deleteQueue = [];
+
+    // filter all predictors with this name or date (columns or rows)
+    const predictorFilter = this.predictors.filter(function(obj) {
+      return obj[filterType] == name;
+    });
+
+    // get ids of each predictor to delete
+    for(let i=0; i < predictorFilter.length; i++) {
+      deleteQueue.push(predictorFilter[i]['id']);
+    }
+
+    // Delete predictors
+    this.predictorsService.deletePredictors(deleteQueue);
+  }
+
   public onPageChange(pageNum: number): void {
     this.pageSize = this.itemsPerPage*(pageNum - 1);
   }
--- src/app/utility/predictors/predictors.service.ts
+++ src/app/utility/predictors/predictors.service.ts
@@ -1,10 +1,8 @@
-import { ElementRef, QueryList, ViewChildren, Injectable } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Injectable } from '@angular/core';
 import { Observable, BehaviorSubject } from 'rxjs';
 import { AccountService } from "../../account/account/account.service";
 import { FacilityService } from 'src/app/account/facility/facility.service';
 import { PredictordbService } from 'src/app/indexedDB/predictors-db.service';
-import { LoadingService } from "../../shared/loading/loading.service";
 
 
 @Injectable({
@@ -19,7 +17,6 @@ export class PredictorsService {
   constructor(
     private accountService: AccountService,
     private facilityService: FacilityService,
-    private loadingService: LoadingService,
     private predictordbService: PredictordbService,
   ) { 
      // Observe the accountid var
@@ -34,8 +31,7 @@ export class PredictorsService {
 
   }
 
-
-  getPredictors(): Observable<any> {
+  getPredictors(): Observable<Array<object>> {
     // If var is empty, run query
     if (this.predictors.value.length == 0) {
       this.setPredictors();
@@ -56,42 +52,67 @@ export class PredictorsService {
     );
   }
 
-  addPredictor(dates) {
-    // Add new header
-    const name = "NAME " + Math.floor(Math.random() * 100);
+  addPredictor(dates, item): void {
+    // Generate temp name
+    const name = "New Predictor " + (item + 1);
 
-    // add all backlog rows
-        // Count how many in backlog
-        // Get list of all month/years in system
-        // Loop though list, add 
-    //const backlog = [];
-    
+    // Add all of the backlog dates to keep table uniform
     for(var date of dates) {
       this.predictordbService.add(name,this.facilityid,this.accountid,date).then(
-        data => {
-          this.setPredictors(); // Refresh list of predictors
-        },
+        data => {},
         error => {
             console.log(error);
         }
       );
     }
+
+    // Refresh list of predictors
+    this.setPredictors(); 
   }
 
-  addPredictorRow(currentPredictors, date) {
-    // for each header, 
-    // add new item
+  addPredictorRow(currentPredictors, date): void {
+    // for each header, add new item
     for(var header of currentPredictors) {
 
       this.predictordbService.add(header,this.facilityid,this.accountid,date).then(
-        data => {
-          this.setPredictors(); // Refresh list of predictors
-        },
+        data => {},
         error => {
             console.log(error);
         }
       );
     }
+    
+    // Refresh list of predictors
+    this.setPredictors(); 
+  }
+
+  deletePredictors(deleteQueue): void {
+    // loop and delete each
+    for(let i=0; i < deleteQueue.length; i++) {
+      this.predictordbService.deleteIndex(deleteQueue[i]);
+    }
+
+    // Refresh list of predictors
+    this.setPredictors(); 
+  }
+
+
+  updatePredictor(value): void {
+    for (let i=0; i<value.length; i++) {
+      this.predictordbService.update(value[i]).then(
+        data => {},
+        error => {
+            console.log(error);
+        }
+      );
+    }
+
+    // Refresh list of predictors
+    this.setPredictors(); 
+  }
+
+  getAllByName(name) {
+    return this.predictordbService.getAllByName(name);
   }
 
 }
