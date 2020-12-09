f83bd1260d62fd8650d2f40556bbec9886e3a5c4
mark-root <mroot@intertech.com>
2020-12-09 13:37:17 -0600

    setting heat capacity and site to source with conversion

--- src/app/utility/energy-consumption/energy-source/edit-meter-form/edit-meter-form.component.ts
+++ src/app/utility/energy-consumption/energy-source/edit-meter-form/edit-meter-form.component.ts
@@ -1,8 +1,11 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FacilitydbService } from 'src/app/indexedDB/facility-db.service';
 import { UtilityMeterdbService } from 'src/app/indexedDB/utilityMeter-db.service';
-import { IdbUtilityMeter } from 'src/app/models/idb';
-import { GasOptions, LiquidOptions, SolidOptions, OtherEnergyOptions, GasUnits, LiquidUnits, SolidUnits, ElectricityUnits, FuelTypeOption } from './editMeterOptions';
+import { IdbFacility, IdbUtilityMeter } from 'src/app/models/idb';
+import { ConvertUnitsService } from 'src/app/shared/convert-units/convert-units.service';
+import { EnergyUnitOptions, MassUnitOptions, UnitOption, VolumeUnitOptions } from 'src/app/shared/unitOptions';
+import { GasOptions, LiquidOptions, SolidOptions, OtherEnergyOptions, FuelTypeOption } from './editMeterOptions';
 @Component({
   selector: 'app-edit-meter-form',
   templateUrl: './edit-meter-form.component.html',
@@ -18,9 +21,9 @@ export class EditMeterFormComponent implements OnInit {
   displayPhase: boolean;
   displayFuel: boolean;
   energyOptions: Array<FuelTypeOption>;
-  startingUnitOptions: Array<string>;
+  startingUnitOptions: Array<UnitOption>;
   energySourceLabel: string = 'Fuel Type';
-  constructor(private formBuilder: FormBuilder, private utilityMeterDbService: UtilityMeterdbService) { }
+  constructor(private formBuilder: FormBuilder, private utilityMeterDbService: UtilityMeterdbService, private facilityDbService: FacilitydbService, private convertUnitsService: ConvertUnitsService) { }
 
   ngOnInit(): void {
     this.meterForm = this.formBuilder.group({
@@ -44,7 +47,7 @@ export class EditMeterFormComponent implements OnInit {
       fuel: [this.editMeter.fuel, Validators.required],
       startingUnit: [this.editMeter.startingUnit, Validators.required]
     });
-    this.changeSource();
+    this.setSource();
   }
 
 
@@ -57,7 +60,7 @@ export class EditMeterFormComponent implements OnInit {
     this.emitClose.emit(true);
   }
 
-  changeSource() {
+  setSource() {
     this.setSourceValidation();
     this.setEnergyOptions();
   }
@@ -65,7 +68,7 @@ export class EditMeterFormComponent implements OnInit {
   setSourceValidation() {
     if (this.meterForm.controls.source.value == 'Other Fuels') {
       this.meterForm.controls.phase.setValidators([Validators.required]);
-      if(!this.meterForm.controls.phase.value){
+      if (!this.meterForm.controls.phase.value) {
         this.meterForm.controls.phase.patchValue('Gas');
       }
       this.displayPhase = true;
@@ -91,13 +94,10 @@ export class EditMeterFormComponent implements OnInit {
       this.energySourceLabel = 'Fuel Type';
       if (this.meterForm.controls.phase.value == 'Solid') {
         this.energyOptions = SolidOptions;
-        this.startingUnitOptions = SolidUnits;
       } else if (this.meterForm.controls.phase.value == 'Liquid') {
         this.energyOptions = LiquidOptions;
-        this.startingUnitOptions = LiquidUnits;
       } else if (this.meterForm.controls.phase.value == 'Gas') {
         this.energyOptions = GasOptions;
-        this.startingUnitOptions = GasUnits;
       }
     } else if (this.meterForm.controls.source.value == 'Water' || this.meterForm.controls.source.value == 'Waste Water') {
       this.energySourceLabel = 'Water Type'
@@ -105,14 +105,43 @@ export class EditMeterFormComponent implements OnInit {
     } else if (this.meterForm.controls.source.value == 'Other Energy') {
       this.energySourceLabel = 'Energy Type'
       this.energyOptions = OtherEnergyOptions;
-    } else if (this.meterForm.controls.source.value == 'Electricity') {
-      this.startingUnitOptions = ElectricityUnits;
+    }
+    this.setUnitOptions();
+  }
+
+  //TODO: Set heat capacity and source for electricity and natural gas (no energy options used);
+  setFuelType() {
+    let selectedEnergyOption: FuelTypeOption = JSON.parse(JSON.stringify(this.energyOptions.find(option => { return option.value == this.meterForm.controls.fuel.value })));
+    if (selectedEnergyOption) {
+      //TODO: Add therms to unit converter
+      if (selectedEnergyOption.startingUnit != 'Therms') {
+        //TODO: Round value to some decimal place
+        selectedEnergyOption.heatCapacityValue = this.convertUnitsService.value(selectedEnergyOption.heatCapacityValue).from(selectedEnergyOption.startingUnit).to(this.meterForm.controls.startingUnit.value);
+      }
+      this.meterForm.controls.heatCapacity.patchValue(selectedEnergyOption.heatCapacityValue);
+      this.meterForm.controls.siteToSource.patchValue(selectedEnergyOption.siteToSourceMultiplier);
     }
   }
 
-  setFuelType() {
-    let selectedEnergyOption: FuelTypeOption = this.energyOptions.find(option => { return option.value == this.meterForm.controls.fuel.value });
-    this.meterForm.controls.heatCapacity.patchValue(selectedEnergyOption.conversionToMMtbu);
-    this.meterForm.controls.siteToSource.patchValue(selectedEnergyOption.siteToSourceMultiplier);
+  setUnitOptions() {
+    let selectedFacility: IdbFacility = this.facilityDbService.selectedFacility.getValue();
+    if (this.meterForm.controls.source.value == 'Electricity' || this.meterForm.controls.source.value == 'Other Energy' || this.meterForm.controls.source.value == 'Natural Gas') {
+      this.meterForm.controls.startingUnit.patchValue(selectedFacility.energyUnit);
+      this.startingUnitOptions = EnergyUnitOptions;
+    } else if (this.meterForm.controls.source.value == 'Water' || this.meterForm.controls.source.value == 'Waste Water') {
+      this.meterForm.controls.startingUnit.patchValue(selectedFacility.volumeUnit);
+      this.startingUnitOptions = VolumeUnitOptions;
+    } else if (this.meterForm.controls.source.value == 'Other Fuels') {
+      if (this.meterForm.controls.phase.value == 'Solid') {
+        this.meterForm.controls.startingUnit.patchValue(selectedFacility.massUnit);
+        this.startingUnitOptions = MassUnitOptions;
+      } else if (this.meterForm.controls.phase.value == 'Liquid') {
+        this.meterForm.controls.startingUnit.patchValue(selectedFacility.volumeUnit);
+        this.startingUnitOptions = VolumeUnitOptions;
+      } else if (this.meterForm.controls.phase.value == 'Gas') {
+        this.meterForm.controls.startingUnit.patchValue(selectedFacility.volumeUnit);
+        this.startingUnitOptions = VolumeUnitOptions;
+      }
+    }
   }
 }
--- src/app/utility/energy-consumption/energy-source/edit-meter-form/editMeterOptions.ts
+++ src/app/utility/energy-consumption/energy-source/edit-meter-form/editMeterOptions.ts
@@ -1,56 +1,56 @@
 export interface FuelTypeOption {
     startingUnit: string,
-    conversionToMMtbu: number
+    heatCapacityValue: number
     value: string,
     siteToSourceMultiplier: number
 }
 
 export const GasOptions: Array<FuelTypeOption> = [
     {
-        startingUnit: 'SCF',
-        conversionToMMtbu: .00009,
+        startingUnit: 'ft3',
+        heatCapacityValue: .00009,
         value: 'Blast Furnace Gas',
         siteToSourceMultiplier: 1
     },
     {
-        startingUnit: 'SCF',
-        conversionToMMtbu: .00059,
+        startingUnit: 'ft3',
+        heatCapacityValue: .00059,
         value: 'Coke Oven Gas',
         siteToSourceMultiplier: 1
     },
     {
-        startingUnit: 'SCF',
-        conversionToMMtbu: .0027,
+        startingUnit: 'ft3',
+        heatCapacityValue: .0027,
         value: 'Liquefied Petroleum Gas (LPG)',
         siteToSourceMultiplier: 1
     },
     {
-        startingUnit: 'SCF',
-        conversionToMMtbu: .002816,
+        startingUnit: 'ft3',
+        heatCapacityValue: .002816,
         value: 'Propane',
         siteToSourceMultiplier: 1
     },
     {
-        startingUnit: 'SCF',
-        conversionToMMtbu: .00328,
+        startingUnit: 'ft3',
+        heatCapacityValue: .00328,
         value: 'Butane',
         siteToSourceMultiplier: 1
     },
     {
-        startingUnit: 'SCF',
-        conversionToMMtbu: .0031,
+        startingUnit: 'ft3',
+        heatCapacityValue: .0031,
         value: 'Isobutane',
         siteToSourceMultiplier: 1
     },
     {
-        startingUnit: 'SCF',
-        conversionToMMtbu: .0006,
+        startingUnit: 'ft3',
+        heatCapacityValue: .0006,
         value: 'Landfill Gas',
         siteToSourceMultiplier: 1
     },
     {
-        startingUnit: 'SCF',
-        conversionToMMtbu: .0007,
+        startingUnit: 'ft3',
+        heatCapacityValue: .0007,
         value: 'Oil Gases',
         siteToSourceMultiplier: 1
     }
@@ -59,187 +59,187 @@ export const GasOptions: Array<FuelTypeOption> = [
 export const LiquidOptions: Array<FuelTypeOption> = [
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .0916476,
+        heatCapacityValue: .0916476,
         value: 'Propane',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .0916476,
+        heatCapacityValue: .0916476,
         value: 'LPG',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .13869,
+        heatCapacityValue: .13869,
         value: 'Distillate Fuel Oil',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .14969,
+        heatCapacityValue: .14969,
         value: 'Residual Fuel Oil',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .13869,
+        heatCapacityValue: .13869,
         value: 'Heating Oil',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .13869,
+        heatCapacityValue: .13869,
         value: 'Diesel',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .102032,
+        heatCapacityValue: .102032,
         value: 'Butane',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .10375,
+        heatCapacityValue: .10375,
         value: 'Isobutane',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .1108,
+        heatCapacityValue: .1108,
         value: 'Pentane',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .012072,
+        heatCapacityValue: .012072,
         value: 'Ethylene',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .017473,
+        heatCapacityValue: .017473,
         value: 'Propylene',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .023008,
+        heatCapacityValue: .023008,
         value: 'Butene',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .028693,
+        heatCapacityValue: .028693,
         value: 'Pentene',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .028057,
+        heatCapacityValue: .028057,
         value: 'Benzene',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .03354,
+        heatCapacityValue: .03354,
         value: 'Toluene',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .03912,
+        heatCapacityValue: .03912,
         value: 'Xylene',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .006492,
+        heatCapacityValue: .006492,
         value: 'Methyl Alcohol',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .011968,
+        heatCapacityValue: .011968,
         value: 'Ethyl Alcohol',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .1374,
+        heatCapacityValue: .1374,
         value: 'Fuel Oil #1',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .1396,
+        heatCapacityValue: .1396,
         value: 'Fuel Oil #2',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .1451,
+        heatCapacityValue: .1451,
         value: 'Fuel Oil #4',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .1488,
+        heatCapacityValue: .1488,
         value: 'Fuel Oil #5',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .1524,
+        heatCapacityValue: .1524,
         value: 'Fuel Oil #6 (high sulfur)',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .149,
+        heatCapacityValue: .149,
         value: 'Fuel Oil #6 (low sulfur)',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .1387,
+        heatCapacityValue: .1387,
         value: 'Crude Oil',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .12760,
+        heatCapacityValue: .12760,
         value: 'Gasoline',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .1351,
+        heatCapacityValue: .1351,
         value: 'Kerosene',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .138,
+        heatCapacityValue: .138,
         value: 'Gas Oil',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .086,
+        heatCapacityValue: .086,
         value: 'Liquefied Natural Gas (LNG)',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .127,
+        heatCapacityValue: .127,
         value: 'Biodiesel (100%)',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'gal',
-        conversionToMMtbu: .084262,
+        heatCapacityValue: .084262,
         value: 'Ethanol (100%)',
         siteToSourceMultiplier: 1
     }
@@ -248,62 +248,62 @@ export const LiquidOptions: Array<FuelTypeOption> = [
 
 export const SolidOptions: Array<FuelTypeOption> = [
     {
-        startingUnit: 'Tons',
-        conversionToMMtbu: 25.09,
+        startingUnit: 'ton',
+        heatCapacityValue: 25.09,
         value: 'Coal (anthracite)',
         siteToSourceMultiplier: 1
     },
     {
-        startingUnit: 'Tons',
-        conversionToMMtbu: 24.93,
+        startingUnit: 'ton',
+        heatCapacityValue: 24.93,
         value: 'Coal (bituminous)',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'lb',
-        conversionToMMtbu: .014,
+        heatCapacityValue: .014,
         value: 'Coal',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'lb',
-        conversionToMMtbu: .013,
+        heatCapacityValue: .013,
         value: 'Coke',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'lb',
-        conversionToMMtbu: .006,
+        heatCapacityValue: .006,
         value: 'Peat',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'lb',
-        conversionToMMtbu: .008,
+        heatCapacityValue: .008,
         value: 'Wood',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'lb',
-        conversionToMMtbu: .009,
+        heatCapacityValue: .009,
         value: 'Biomass',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'lb',
-        conversionToMMtbu: .0065,
+        heatCapacityValue: .0065,
         value: 'Black Liquor',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'lb',
-        conversionToMMtbu: .016,
+        heatCapacityValue: .016,
         value: 'Scrap Tires',
         siteToSourceMultiplier: 1
     },
     {
         startingUnit: 'lb',
-        conversionToMMtbu: .004,
+        heatCapacityValue: .004,
         value: 'Sulfur',
         siteToSourceMultiplier: 1
     }
@@ -312,65 +312,38 @@ export const SolidOptions: Array<FuelTypeOption> = [
 export const OtherEnergyOptions: Array<FuelTypeOption> = [
     {
         startingUnit: 'lb',
-        conversionToMMtbu: .001194,
+        heatCapacityValue: .001194,
         value: 'District Steam (150 psig)',
         siteToSourceMultiplier: undefined
     },
     {
         startingUnit: 'lb',
-        conversionToMMtbu: undefined,
+        heatCapacityValue: undefined,
         value: 'Purchased Steam',
         siteToSourceMultiplier: 1.33
     },
     {
         startingUnit: 'lb',
-        conversionToMMtbu: .012,
+        heatCapacityValue: .012,
         value: 'Purchased Chilled Water (Absorption Chiller)',
         siteToSourceMultiplier: 1.25
     },
     {
         startingUnit: 'lb',
-        conversionToMMtbu: .012,
+        heatCapacityValue: .012,
         value: 'Purchased Chilled Water (Electric-driven Compressor)',
         siteToSourceMultiplier: .24
     },
     {
         startingUnit: 'lb',
-        conversionToMMtbu: .012,
-        value: 'Purchased Chilled Water (Enginer-driven Compressor)',
+        heatCapacityValue: .012,
+        value: 'Purchased Chilled Water (Engine-driven Compressor)',
         siteToSourceMultiplier: .83
     },
     {
         startingUnit: 'Therms',
-        conversionToMMtbu: .1,
+        heatCapacityValue: .1,
         value: 'District Hot Water',
         siteToSourceMultiplier: undefined
     }
-]
-
-export const FuelOptions: Array<string> = [
-
-]
-
-
-export const ElectricityUnits: Array<string> = [
-    'kWh',
-    'MWh'
-]
-
-export const GasUnits: Array<string> = [
-    'SCF',
-    'lbs'
-]
-
-export const SolidUnits: Array<string> = [
-    'Tons',
-    'lbs'
-]
-
-export const LiquidUnits: Array<string> = [
-    'gal',
-    'Ton/hr',
-    'gal-F',
-    'Therms'
 ]
