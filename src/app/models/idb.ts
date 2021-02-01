72237c5231dcad343b86c5decfcadaa0b5d07f4b
mark-root <mroot@intertech.com>
2021-02-01 13:26:15 -0600

    update validation and units for meter data forms

--- src/app/models/idb.ts
+++ src/app/models/idb.ts
@@ -96,7 +96,7 @@ export interface IdbUtilityMeterData {
     accountId: number,
     //data
     readDate: Date,
-    unit: string,
+    // unit: string,
     totalVolume: number,
     totalEnergyUse: number,
     totalCost: number,
