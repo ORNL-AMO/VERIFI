9f2e8270c31e5192a239536291f71eee6b8054b1
mark-root <mroot@intertech.com>
2021-02-12 15:10:47 -0600

    parsing multiple files and getting meter file summarries

---
+++ src/app/utility/upload-data/import-meter.service.ts
@@ -0,0 +1,157 @@
+import { Injectable } from '@angular/core';
+import { UtilityMeterdbService } from 'src/app/indexedDB/utilityMeter-db.service';
+import { IdbFacility, IdbUtilityMeter } from 'src/app/models/idb';
+import { CsvImportData } from 'src/app/shared/helper-services/csv-to-json.service';
+import { EnergyUnitsHelperService } from 'src/app/shared/helper-services/energy-units-helper.service';
+import { EnergyUseCalculationsService } from 'src/app/shared/helper-services/energy-use-calculations.service';
+import { UnitOption } from 'src/app/shared/unitOptions';
+import { EditMeterFormService } from '../energy-consumption/energy-source/edit-meter-form/edit-meter-form.service';
+import { FuelTypeOption, SourceOptions } from '../energy-consumption/energy-source/edit-meter-form/editMeterOptions';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImportMeterService {
+
+  constructor(private energyUnitsHelperService: EnergyUnitsHelperService, private energyUseCalculationsService: EnergyUseCalculationsService,
+    private utilityMeterdbService: UtilityMeterdbService, private editMeterFormService: EditMeterFormService) { }
+
+  importMetersFromDataFile(csvImportData: CsvImportData, selectedFacility: IdbFacility, facilityMeters: Array<IdbUtilityMeter>): ImportMeterFileSummary {
+    let existingMeters: Array<IdbUtilityMeter> = new Array();
+    let newMeters: Array<IdbUtilityMeter> = new Array();
+    let invalidMeters: Array<IdbUtilityMeter> = new Array();
+    csvImportData.data.forEach(dataItem => {
+      let checkHasData: boolean = false;
+      Object.keys(dataItem).forEach((key) => {
+        if (dataItem[key] != null) {
+          checkHasData = true;
+        }
+      });
+      if (checkHasData) {
+        let importMeter: ImportMeter = this.parseDataItem(dataItem);
+        let newMeter: IdbUtilityMeter = this.getNewMeterFromImportMeter(importMeter, selectedFacility);
+        let importMeterStatus: string = this.getImportMeterStatus(newMeter, facilityMeters);
+        if (importMeterStatus == "new") {
+          newMeters.push(newMeter);
+        } else if (importMeterStatus == "existing") {
+          existingMeters.push(newMeter);
+        } else if (importMeterStatus == "invalid") {
+          invalidMeters.push(newMeter);
+        }
+      }
+    });
+    return {
+      existingMeters: existingMeters,
+      newMeters: newMeters,
+      invalidMeters: invalidMeters
+    }
+  }
+
+  parseDataItem(dataItem: any): ImportMeter {
+    return {
+      accountNumber: dataItem["Account Number"],
+      location: dataItem["Building / Location"],
+      collectionUnit: dataItem["Collection Unit"],
+      fuel: dataItem["Fuel"],
+      heatCapacity: dataItem["Heat Capacity"],
+      meterGroup: dataItem["Meter Group"],
+      meterName: dataItem["Meter Name"],
+      meterNumber: dataItem["Meter Number"],
+      notes: dataItem["Notes"],
+      phase: dataItem["Phase"],
+      siteToSource: dataItem["Site To Source"],
+      source: dataItem["Source"],
+      utilitySupplier: dataItem["Utility Supplier"]
+    }
+  }
+
+
+  getNewMeterFromImportMeter(importMeter: ImportMeter, selectedFacility: IdbFacility): IdbUtilityMeter {
+    let newMeter: IdbUtilityMeter = this.utilityMeterdbService.getNewIdbUtilityMeter(selectedFacility.id, selectedFacility.accountId);
+    newMeter.meterNumber = importMeter.meterNumber;
+    newMeter.accountNumber = importMeter.accountNumber;
+    newMeter.source = this.checkImportSource(importMeter.source);
+    newMeter.phase = this.checkImportPhase(importMeter.phase);
+    newMeter.name = importMeter.meterName;
+    newMeter.supplier = importMeter.utilitySupplier;
+    newMeter.notes = importMeter.notes;
+    newMeter.location = importMeter.location;
+    newMeter.group = importMeter.meterGroup;
+    newMeter.fuel = importMeter.fuel;
+    newMeter.startingUnit = this.checkImportStartingUnit(importMeter.collectionUnit, newMeter.source, newMeter.phase, newMeter.fuel);
+    newMeter.heatCapacity = importMeter.heatCapacity;
+    newMeter.siteToSource = importMeter.siteToSource;
+    return newMeter;
+  }
+
+
+  checkImportSource(source: string): string {
+    let selectedSource: string = SourceOptions.find(sourceOption => { return sourceOption == source });
+    return selectedSource;
+  }
+
+  checkImportPhase(phase: string): string {
+    if (phase == 'Gas' || phase == 'Liquid' || phase == 'Solid') {
+      return phase;
+    }
+    return undefined;
+  }
+
+  checkImportStartingUnit(importUnit: string, source: string, phase: string, fuel: string): string {
+    if (source) {
+      let startingUnitOptions: Array<UnitOption> = this.energyUnitsHelperService.getStartingUnitOptions(source, phase, fuel);
+      let selectedUnitOption: UnitOption = startingUnitOptions.find(unitOption => { return unitOption.value == importUnit });
+      if (selectedUnitOption) {
+        return selectedUnitOption.value;
+      }
+    }
+    return undefined;
+  }
+
+  checkImportFuel(fuel: string, source: string, phase: string): string {
+    let fuelTypeOptions = this.energyUseCalculationsService.getFuelTypeOptions(source, phase);
+    let selectedEnergyOption: FuelTypeOption = fuelTypeOptions.find(option => { return option.value == fuel });
+    if (selectedEnergyOption) {
+      return selectedEnergyOption.value;
+    }
+    return undefined;
+  }
+
+  getImportMeterStatus(importMeter: IdbUtilityMeter, facilityMeters: Array<IdbUtilityMeter>): "invalid" | "existing" | "new" {
+    let isMeterInvalid: boolean = this.editMeterFormService.getFormFromMeter(importMeter).invalid;
+    if (isMeterInvalid) {
+      return 'invalid';
+    } else {
+      let facilityMeter: IdbUtilityMeter = facilityMeters.find(facilityMeter => { return facilityMeter.name == importMeter.name });
+      if (facilityMeter) {
+        return 'existing';
+      } else {
+        return 'new';
+      }
+    }
+  }
+
+}
+
+
+export interface ImportMeter {
+  accountNumber: number,
+  location: string,
+  collectionUnit: string,
+  fuel: string,
+  heatCapacity: number,
+  meterGroup: string,
+  meterName: string,
+  meterNumber: string,
+  notes: string,
+  phase: string,
+  siteToSource: number,
+  source: string,
+  utilitySupplier: string
+}
+
+export interface ImportMeterFileSummary {
+  existingMeters: Array<IdbUtilityMeter>,
+  newMeters: Array<IdbUtilityMeter>,
+  invalidMeters: Array<IdbUtilityMeter>
+}
--- src/app/utility/upload-data/upload-data.component.ts
+++ src/app/utility/upload-data/upload-data.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FacilitydbService } from 'src/app/indexedDB/facility-db.service';
+import { UtilityMeterdbService } from 'src/app/indexedDB/utilityMeter-db.service';
+import { IdbFacility, IdbUtilityMeter } from 'src/app/models/idb';
 import { CsvImportData, CsvToJsonService } from 'src/app/shared/helper-services/csv-to-json.service';
 import { LoadingService } from 'src/app/shared/loading/loading.service';
+import { ImportMeterService, ImportMeterFileSummary } from './import-meter.service';
+import { ElectricityMeterDataHeaders, MeterHeaders, NonElectricityMeterDataHeaders } from './templateHeaders';
 
 @Component({
   selector: 'app-upload-data',
@@ -9,60 +14,114 @@ import { LoadingService } from 'src/app/shared/loading/loading.service';
 })
 export class UploadDataComponent implements OnInit {
 
-  fileReference: any;
-  validFile: boolean;
-  importData: any = null;
-  previewDataFromCsv: CsvImportData;
-  importDataFromCsv: CsvImportData;
-  importingData: boolean = false;
-  importSuccesful: boolean = false;
-  selectedHeaderRow: number;
-  constructor(private csvToJsonService: CsvToJsonService, private loadingService: LoadingService) { }
+  importMeterFiles: Array<{fileName: string, importMeterFileSummary: ImportMeterFileSummary}>;
+
+
+  filesSummary: Array<{ fileType: string, fileData: CsvImportData, fileName: string }>;
+  fileReferences: Array<any>;
+  nonTemplateFiles: Array<{ fileType: string, fileData: CsvImportData, fileName: string }>;
+  inputLabel: string = 'Browse Files'
+  constructor(private csvToJsonService: CsvToJsonService, private loadingService: LoadingService,
+    private importMeterService: ImportMeterService, private facilityDbService: FacilitydbService,
+    private utilityMeterDbService: UtilityMeterdbService) { }
 
   ngOnInit(): void {
+    this.initArrays();
+  }
+
+  initArrays(){
+    this.importMeterFiles = new Array();
+    this.filesSummary = new Array();
+    this.nonTemplateFiles = new Array();
   }
 
   setImportFile(files: FileList) {
+    this.fileReferences = new Array();
+    this.filesSummary = new Array();
     if (files) {
       if (files.length !== 0) {
         let regex = /.csv$/;
         let regex2 = /.CSV$/;
-        if (regex.test(files[0].name) || regex2.test(files[0].name)) {
-          this.fileReference = files[0];
-          this.validFile = true;
-          this.importFile();
-        } else {
-          this.validFile = false;
+        for (let index = 0; index < files.length; index++) {
+          const element = files[index];
+          if (regex.test(files[index].name) || regex2.test(files[index].name)) {
+            this.fileReferences.push(files[index]);
+          }
+        }
+        if (this.fileReferences.length != 0) {
+          this.importFiles();
         }
       }
     }
   }
 
-  importFile() {
-    let fr: FileReader = new FileReader();
-    fr.readAsText(this.fileReference);
-    fr.onloadend = (e) => {
-      this.importData = JSON.parse(JSON.stringify(fr.result));
-      let test = this.csvToJsonService.parseCsvHeaders(this.importData);
-      console.log(test);
-    };
+  importFiles() {
+    this.fileReferences.forEach(fileReference => {
+      let fr: FileReader = new FileReader();
+      fr.readAsText(fileReference);
+      fr.onloadend = (e) => {
+        let importData: any = JSON.parse(JSON.stringify(fr.result));
+        let fileHeaders: Array<string> = this.csvToJsonService.parseCsvHeaders(importData);
+        this.addFile(importData, fileHeaders, fileReference.name);
+      };
+    })
   }
 
 
-  parsePreviewData() {
-    this.previewDataFromCsv = this.csvToJsonService.parseCsvWithoutHeaders(this.importData);
+  addFile(data: any, fileHeaders: Array<string>, fileName: string) {
+    let selectedFacility: IdbFacility = this.facilityDbService.selectedFacility.getValue();
+    let facilityMeters: Array<IdbUtilityMeter> = this.utilityMeterDbService.facilityMeters.getValue();
+    if (JSON.stringify(fileHeaders) === JSON.stringify(MeterHeaders)) {
+      let fileData: CsvImportData = this.csvToJsonService.parseCsvWithHeaders(data, 0);
+      let summary: ImportMeterFileSummary = this.importMeterService.importMetersFromDataFile(fileData, selectedFacility, facilityMeters)
+      //meter template
+      this.importMeterFiles.push({
+        fileName: fileName,
+        importMeterFileSummary: summary
+      });
+
+    } else if (JSON.stringify(fileHeaders) === JSON.stringify(ElectricityMeterDataHeaders)) {
+      //electricity meter data template 
+      this.filesSummary.push({
+        fileType: 'Meter Data',
+        fileData: this.csvToJsonService.parseCsvWithHeaders(data, 0),
+        fileName: fileName
+      });
+
+    } else if (JSON.stringify(fileHeaders) === JSON.stringify(NonElectricityMeterDataHeaders)) {
+      //non electricity meter data template
+      this.filesSummary.push({
+        fileType: 'Meter Data',
+        fileData: this.csvToJsonService.parseCsvWithHeaders(data, 0),
+        fileName: fileName
+      });
+
+    } else {
+      //other .csv file
+      this.nonTemplateFiles.push({
+        fileType: undefined,
+        fileData: this.csvToJsonService.parseCsvWithHeaders(data, 0),
+        fileName: fileName
+      })
+    }
+
   }
 
-  parseImportData() {
-    this.loadingService.setLoadingMessage('Parsing CSV');
-    this.loadingService.setLoadingStatus(true);
-    this.importingData = true;
-    this.previewDataFromCsv = undefined;
-    this.importDataFromCsv = this.csvToJsonService.parseCsvWithHeaders(this.importData, Number(this.selectedHeaderRow));
-    this.importSuccesful = true;
-    this.importData = undefined;
-    this.loadingService.setLoadingStatus(false);
-    this.loadingService.setLoadingMessage(undefined);
-  }
+
+  // parsePreviewData() {
+  //   this.previewDataFromCsv = this.csvToJsonService.parseCsvWithoutHeaders(this.importData);
+  // }
+
+  // parseImportData() {
+  //   this.loadingService.setLoadingMessage('Parsing CSV');
+  //   this.loadingService.setLoadingStatus(true);
+  //   this.importingData = true;
+  //   this.previewDataFromCsv = undefined;
+  //   this.importDataFromCsv = this.csvToJsonService.parseCsvWithHeaders(this.importData, Number(this.selectedHeaderRow));
+  //   this.importSuccesful = true;
+  //   this.importData = undefined;
+  //   this.loadingService.setLoadingStatus(false);
+  //   this.loadingService.setLoadingMessage(undefined);
+  // }
 
 }
