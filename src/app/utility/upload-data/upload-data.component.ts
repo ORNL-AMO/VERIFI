be93485ad29fe63c62a9c1500e83482d744de10d
mark-root <mroot@intertech.com>
2021-02-15 12:31:22 -0600

    import xlsx started

--- src/app/utility/upload-data/upload-data.component.ts
+++ src/app/utility/upload-data/upload-data.component.ts
@@ -6,6 +6,9 @@ import { CsvImportData, CsvToJsonService } from 'src/app/shared/helper-services/
 import { LoadingService } from 'src/app/shared/loading/loading.service';
 import { ImportMeterService, ImportMeterFileSummary } from './import-meter.service';
 import { ElectricityMeterDataHeaders, MeterHeaders, NonElectricityMeterDataHeaders } from './templateHeaders';
+import * as XLSX from 'xlsx';
+
+type AOA = any[][];
 
 @Component({
   selector: 'app-upload-data',
@@ -14,13 +17,16 @@ import { ElectricityMeterDataHeaders, MeterHeaders, NonElectricityMeterDataHeade
 })
 export class UploadDataComponent implements OnInit {
 
-  importMeterFiles: Array<{fileName: string, importMeterFileSummary: ImportMeterFileSummary}>;
+  importMeterFiles: Array<{ fileName: string, importMeterFileSummary: ImportMeterFileSummary }>;
 
 
   filesSummary: Array<{ fileType: string, fileData: CsvImportData, fileName: string }>;
   fileReferences: Array<any>;
   nonTemplateFiles: Array<{ fileType: string, fileData: CsvImportData, fileName: string }>;
   inputLabel: string = 'Browse Files'
+
+  excelFiles: Array<any>;
+
   constructor(private csvToJsonService: CsvToJsonService, private loadingService: LoadingService,
     private importMeterService: ImportMeterService, private facilityDbService: FacilitydbService,
     private utilityMeterDbService: UtilityMeterdbService) { }
@@ -29,22 +35,24 @@ export class UploadDataComponent implements OnInit {
     this.initArrays();
   }
 
-  initArrays(){
+  initArrays() {
     this.importMeterFiles = new Array();
     this.filesSummary = new Array();
     this.nonTemplateFiles = new Array();
+    this.excelFiles = new Array();
+    this.fileReferences = new Array();
   }
 
   setImportFile(files: FileList) {
-    this.fileReferences = new Array();
-    this.filesSummary = new Array();
+    this.initArrays();
     if (files) {
       if (files.length !== 0) {
         let regex = /.csv$/;
         let regex2 = /.CSV$/;
+        let regex3 = /.xlsx$/;
         for (let index = 0; index < files.length; index++) {
           const element = files[index];
-          if (regex.test(files[index].name) || regex2.test(files[index].name)) {
+          if (regex.test(files[index].name) || regex2.test(files[index].name) || regex3.test(files[index].name)) {
             this.fileReferences.push(files[index]);
           }
         }
@@ -57,13 +65,19 @@ export class UploadDataComponent implements OnInit {
 
   importFiles() {
     this.fileReferences.forEach(fileReference => {
-      let fr: FileReader = new FileReader();
-      fr.readAsText(fileReference);
-      fr.onloadend = (e) => {
-        let importData: any = JSON.parse(JSON.stringify(fr.result));
-        let fileHeaders: Array<string> = this.csvToJsonService.parseCsvHeaders(importData);
-        this.addFile(importData, fileHeaders, fileReference.name);
-      };
+      let excelTest = /.xlsx$/;
+      if (excelTest.test(fileReference.name)) {
+        this.excelFiles.push(fileReference);
+      } else {
+        let fr: FileReader = new FileReader();
+        fr.readAsText(fileReference);
+        fr.onloadend = (e) => {
+          let importData: any = JSON.parse(JSON.stringify(fr.result));
+          let fileHeaders: Array<string> = this.csvToJsonService.parseCsvHeaders(importData);
+          this.addFile(importData, fileHeaders, fileReference.name);
+        };
+
+      }
     })
   }
 
@@ -102,7 +116,7 @@ export class UploadDataComponent implements OnInit {
         fileType: undefined,
         fileData: this.csvToJsonService.parseCsvWithHeaders(data, 0),
         fileName: fileName
-      })
+      });
     }
 
   }
@@ -123,5 +137,34 @@ export class UploadDataComponent implements OnInit {
   //   this.loadingService.setLoadingStatus(false);
   //   this.loadingService.setLoadingMessage(undefined);
   // }
+
+  data: AOA = [[1, 2], [3, 4]];
+  wopts: XLSX.WritingOptions = { bookType: 'xlsx', type: 'array' };
+  fileName: string = 'SheetJS.xlsx';
+
+  onFileChange(evt: any) {
+    /* wire up file reader */
+    const target: DataTransfer = <DataTransfer>(evt.target);
+    if (target.files.length !== 1) throw new Error('Cannot use multiple files');
+    const reader: FileReader = new FileReader();
+    reader.onload = (e: any) => {
+      /* read workbook */
+      const bstr: string = e.target.result;
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary', cellDates: true });
+
+      /* grab first sheet */
+      const wsname: string = wb.SheetNames[1];
+      const ws: XLSX.WorkSheet = wb.Sheets["Sheet1"];
+      var csv = XLSX.utils.sheet_to_csv(wb.Sheets["Sheet1"]);
+      console.log(csv)
+      var XL_row_object = XLSX.utils.sheet_to_json(wb.Sheets["Sheet1"], { header: 1 });
+      console.log(XL_row_object);
+
+
+      /* save data */
+      this.data = <AOA>(XL_row_object);
+    };
+    reader.readAsBinaryString(target.files[0]);
+  }
 
 }
