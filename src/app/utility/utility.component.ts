858d504d86cbc23622c01e814df330389b8ef1fe
Cranfill <cranfillkd@ornl.gov>
2021-03-18 15:35:56 -0400

    Restricting Utility Navigation

--- src/app/utility/utility.component.ts
+++ src/app/utility/utility.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { AccountdbService } from '../indexedDB/account-db.service';
 import { FacilitydbService } from '../indexedDB/facility-db.service';
-import { IdbAccount, IdbFacility } from '../models/idb';
+import { UtilityMeterDatadbService } from '../indexedDB/utilityMeterData-db.service';
+import { IdbAccount, IdbFacility, IdbUtilityMeterData } from '../models/idb';
 
 @Component({
   selector: 'app-utility',
@@ -14,13 +15,16 @@ export class UtilityComponent implements OnInit {
   selectedAccount: IdbAccount;
   selectedFacility: IdbFacility;
   appRendered: boolean;
+  utilityMeterData: Array<IdbUtilityMeterData>;
 
   selectedAccountSub: Subscription;
   selectedFacilitySub: Subscription;
+  utilityDataSub: Subscription;
   
   constructor(
     public accountdbService: AccountdbService,
     public facilityDbService: FacilitydbService,
+    public utilityMeterDataDbService: UtilityMeterDatadbService,
   ) { }
 
   ngOnInit() {
@@ -30,6 +34,10 @@ export class UtilityComponent implements OnInit {
 
     this.selectedFacilitySub = this.facilityDbService.selectedFacility.subscribe(selectedFacility => {
       this.selectedFacility = selectedFacility;
+    });
+
+    this.utilityDataSub = this.utilityMeterDataDbService.facilityMeterData.subscribe(utilityMeterData => {
+      this.utilityMeterData = utilityMeterData;
     });
 
     // TEMP MANUAL DELAY TO PREVENT PAGE FLICKERING.
