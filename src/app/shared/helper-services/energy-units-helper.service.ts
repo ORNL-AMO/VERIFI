417708c40d6d985d201afa93f8b49d724eee023c
mark-root <mroot@intertech.com>
2021-03-25 13:52:20 -0500

    fix setting starting units in meters

--- src/app/shared/helper-services/energy-units-helper.service.ts
+++ src/app/shared/helper-services/energy-units-helper.service.ts
@@ -125,7 +125,7 @@ export class EnergyUnitsHelperService {
     } else if (source == 'Other Energy') {
       let selectedEnergyOption: FuelTypeOption = OtherEnergyOptions.find(option => { return option.value == fuel });
       if (selectedEnergyOption && selectedEnergyOption.otherEnergyType && selectedEnergyOption.otherEnergyType == 'Steam') {
-        return MassUnitOptions.concat(EnergyUnitOptions);
+        return VolumeLiquidOptions.concat(EnergyUnitOptions);
       } else if (selectedEnergyOption && selectedEnergyOption.otherEnergyType && selectedEnergyOption.otherEnergyType == 'Chilled Water') {
         // this.setStartingUnit(selectedFacility.volumeLiquidUnit);
         //TODO: Add chilled water units
