f3fe15faa89f4802fd90e818d5a5aee604ee83f9
Cranfill <cranfillkd@ornl.gov>
2021-04-19 15:23:13 -0400

    Updating Help Panel, Removing text under main component

--- src/app/utility/help-panel/help-panel.component.ts
+++ src/app/utility/help-panel/help-panel.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { FacilitydbService } from 'src/app/indexedDB/facility-db.service';
 
 @Component({
   selector: 'app-help-panel',
@@ -9,16 +11,32 @@ import { NavigationEnd, Router } from '@angular/router';
 export class HelpPanelComponent implements OnInit {
 
   helpText: string;
-
-  constructor(private router: Router) { 
+  selectedSource: string;
+  selectedFacilitySub: Subscription;
+  selectedFacilityName: string = 'Facility';
+  
+  constructor(
+    private router: Router,
+    private facilityDbService: FacilitydbService,
+    ) { 
     router.events.subscribe((route) => {
       if (route instanceof NavigationEnd) {
         this.helpText = route.url.replace('/utility/','');
+        this.selectedSource = this.helpText.split('energy-consumption/')[1];
       }
     });
   }
 
   ngOnInit() {
+    this.selectedFacilitySub = this.facilityDbService.selectedFacility.subscribe(facility => {
+      if (facility) {
+        this.selectedFacilityName = facility.name;
+      }
+    });
+  }
+
+  ngOnDestroy() {
+    this.selectedFacilitySub.unsubscribe();
   }
 
 }
