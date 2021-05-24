077129b7d139bf1deaeb146be0200df3758f1e80
rmroot <mroot@intertech.com>
2021-05-24 15:11:41 -0500

    select which predictors and meters to graph

--- src/app/utility/visualization/matrix-scatter-plot/matrix-scatter-plot.component.ts
+++ src/app/utility/visualization/matrix-scatter-plot/matrix-scatter-plot.component.ts
@@ -20,11 +20,36 @@ export class MatrixScatterPlotComponent implements OnInit {
   @ViewChild('matrixScatterPlot', { static: false }) matrixScatterPlot: ElementRef;
 
   facilityMeterDataSub: Subscription;
+  metersSub: Subscription;
+  meterOptions: Array<{ meter: IdbUtilityMeter, selected: boolean }>;
+
+  facilityPredictorsSub: Subscription;
+  predictorOptions: Array<{ predictor: PredictorData, selected: boolean }>;
 
   constructor(private plotlyService: PlotlyService, private predictorDbService: PredictordbService, private utilityMeterDataDbService: UtilityMeterDatadbService,
     private calanderizationService: CalanderizationService, private utilityMeterDbService: UtilityMeterdbService) { }
 
   ngOnInit(): void {
+    this.facilityPredictorsSub = this.predictorDbService.facilityPredictors.subscribe(predictors => {
+      this.predictorOptions = new Array();
+      predictors.forEach(predictor => {
+        this.predictorOptions.push({
+          predictor: predictor,
+          selected: true
+        });
+      })
+    });
+
+    this.metersSub = this.utilityMeterDbService.facilityMeters.subscribe(facilityMeters => {
+      this.meterOptions = new Array();
+      facilityMeters.forEach(meter => {
+        this.meterOptions.push({
+          meter: meter,
+          selected: true
+        });
+      });
+    });
+
     this.facilityMeterDataSub = this.utilityMeterDataDbService.facilityMeterData.subscribe(() => {
       this.getPlotData();
     });
@@ -32,6 +57,8 @@ export class MatrixScatterPlotComponent implements OnInit {
 
   ngOnDestroy() {
     this.facilityMeterDataSub.unsubscribe();
+    this.facilityPredictorsSub.unsubscribe();
+    this.metersSub.unsubscribe();
   }
 
   ngAfterViewInit() {
@@ -44,23 +71,13 @@ export class MatrixScatterPlotComponent implements OnInit {
 
   drawChart(): void {
     if (this.matrixScatterPlot) {
-
-      var pl_colorscale = [
-        [0.0, '#19d3f3'],
-        [0.333, '#19d3f3'],
-        [0.333, '#e763fa'],
-        [0.666, '#e763fa'],
-        [0.666, '#636efa'],
-        [1, '#636efa']
-      ]
-
-      var axis = () => ({
+      var axis = {
         showline: false,
         zeroline: false,
         gridcolor: '#ffff',
         ticklen: 4,
         // automargin: true
-      });
+      };
 
       let plotData = this.getPlotData();
       console.log(plotData);
@@ -69,8 +86,7 @@ export class MatrixScatterPlotComponent implements OnInit {
         dimensions: plotData,
         text: plotData.map(data => { return data.label }),
         marker: {
-          color: plotData.map(data => { return 1}),
-          // colorscale: pl_colorscale,
+          color: plotData.map(data => { return 1 }),
           autocolorscale: true,
           size: 7,
           // line: {
@@ -78,10 +94,10 @@ export class MatrixScatterPlotComponent implements OnInit {
           //   width: 0.5
           // }
         },
-        diagonal:{
-          visible: false
+        diagonal: {
+          visible: plotData.length < 3
         },
-        showupperhalf: false
+        showupperhalf: plotData.length < 3
       }]
 
       var layout = {
@@ -96,14 +112,14 @@ export class MatrixScatterPlotComponent implements OnInit {
         // hovermode: 'closest',
         // dragmode: 'select',
         plot_bgcolor: 'rgba(240,240,240, 0.95)',
-        xaxis: axis(),
-        yaxis: axis(),
-        xaxis2: axis(),
-        xaxis3: axis(),
-        xaxis4: axis(),
-        yaxis2: axis(),
-        yaxis3: axis(),
-        yaxis4: axis(),
+        xaxis: axis,
+        yaxis: axis,
+        xaxis2: axis,
+        xaxis3: axis,
+        xaxis4: axis,
+        yaxis2: axis,
+        yaxis3: axis,
+        yaxis4: axis,
       }
       var config = { responsive: true };
 
@@ -111,10 +127,23 @@ export class MatrixScatterPlotComponent implements OnInit {
     }
   }
 
+  toggleMeterOption(index: number) {
+    this.meterOptions[index].selected = !this.meterOptions[index].selected;
+    this.drawChart();
+  }
+
+  togglePredictorOption(index: number) {
+    this.predictorOptions[index].selected = !this.predictorOptions[index].selected;
+    this.drawChart();
+  }
 
   getPlotData(): Array<{ label: string, values: Array<number> }> {
-
-    let facilityMeters: Array<IdbUtilityMeter> = this.utilityMeterDbService.facilityMeters.getValue();
+    let facilityMeters: Array<IdbUtilityMeter> = new Array();
+    this.meterOptions.forEach(meterOption => {
+      if (meterOption.selected) {
+        facilityMeters.push(meterOption.meter);
+      }
+    });
     let calanderizedMeterData = this.calanderizationService.getCalanderizedMeterData(facilityMeters, false);
     let lastBillEntry: MonthlyData = this.calanderizationService.getLastBillEntryFromCalanderizedMeterData(calanderizedMeterData);
     let firstBillEntry: MonthlyData = this.calanderizationService.getFirstBillEntryFromCalanderizedMeterData(calanderizedMeterData);
@@ -131,50 +160,73 @@ export class MatrixScatterPlotComponent implements OnInit {
 
 
     let plotData: Array<{ label: string, values: Array<number> }> = new Array();
-    let endDate: Date = _.max([new Date(lastBillEntry.date), new Date(lastPredictorEntry.date)]);
+    let endDate: Date = this.getLastDate(lastBillEntry, lastPredictorEntry);
+
     calanderizedMeterData.forEach(calanderizedMeter => {
-      let startDate: Date = _.min([new Date(firstBillEntry.date), new Date(firstPredictorEntry.date)]);
+      let startDate: Date = this.getLastDate(firstBillEntry, firstPredictorEntry);
       let meterPlotData: { label: string, values: Array<number> } = {
         label: calanderizedMeter.meter.name,
         values: new Array()
       }
-      while (startDate < endDate) {
-        let meterData: MonthlyData = calanderizedMeter.monthlyData.find(dataItem => {
-          return (dataItem.monthNumValue == startDate.getUTCMonth() && dataItem.year == startDate.getUTCFullYear());
-        });
-        if (meterData) {
-          meterPlotData.values.push(meterData.energyUse);
-        } else {
-          meterPlotData.values.push(0);
-        }
-        startDate.setMonth(startDate.getMonth() + 1)
-      };
-      plotData.push(meterPlotData);
+      if (startDate && endDate) {
+        while (startDate < endDate) {
+          let meterData: MonthlyData = calanderizedMeter.monthlyData.find(dataItem => {
+            return (dataItem.monthNumValue == startDate.getUTCMonth() && dataItem.year == startDate.getUTCFullYear());
+          });
+          if (meterData) {
+            meterPlotData.values.push(meterData.energyUse);
+          } else {
+            meterPlotData.values.push(0);
+          }
+          startDate.setMonth(startDate.getMonth() + 1)
+        };
+        plotData.push(meterPlotData);
+      }
     });
 
-    let facilityPredictors: Array<PredictorData> = this.predictorDbService.facilityPredictors.getValue();
+    let facilityPredictors: Array<PredictorData> = new Array();
+    this.predictorOptions.forEach(predictorOption => {
+      if (predictorOption.selected) {
+        facilityPredictors.push(predictorOption.predictor);
+      }
+    })
     facilityPredictors.forEach(predictor => {
-      let startDate: Date = _.min([new Date(firstBillEntry.date), new Date(firstPredictorEntry.date)]);
+      let startDate: Date = this.getLastDate(firstBillEntry, firstPredictorEntry);
       let predictorPlotData: { label: string, values: Array<number> } = {
         label: predictor.name,
         values: new Array()
       }
-      while (startDate < endDate) {
-        let facilityPredictor: IdbPredictorEntry = facilityPredictorEntries.find(dataItem => {
-          let dataItemDate: Date = new Date(dataItem.date);
-          return (dataItemDate.getUTCMonth() == startDate.getUTCMonth() && dataItemDate.getUTCFullYear() == startDate.getUTCFullYear());
-        });
-        if (facilityPredictor) {
-          let predictorData: PredictorData = facilityPredictor.predictors.find(predictorEntry => { return predictorEntry.id == predictor.id });
-          predictorPlotData.values.push(predictorData.amount);
-        } else {
-          predictorPlotData.values.push(0);
-        }
-        startDate.setMonth(startDate.getMonth() + 1)
-      };
-      plotData.push(predictorPlotData);
+      if (startDate && endDate) {
+        while (startDate < endDate) {
+          let facilityPredictor: IdbPredictorEntry = facilityPredictorEntries.find(dataItem => {
+            let dataItemDate: Date = new Date(dataItem.date);
+            return (dataItemDate.getUTCMonth() == startDate.getUTCMonth() && dataItemDate.getUTCFullYear() == startDate.getUTCFullYear());
+          });
+          if (facilityPredictor) {
+            let predictorData: PredictorData = facilityPredictor.predictors.find(predictorEntry => { return predictorEntry.id == predictor.id });
+            predictorPlotData.values.push(predictorData.amount);
+          } else {
+            predictorPlotData.values.push(0);
+          }
+          startDate.setMonth(startDate.getMonth() + 1)
+        };
+        plotData.push(predictorPlotData);
+      }
     });
     return plotData;
+  }
+
+
+  getLastDate(monthlyData?: MonthlyData, predictorEntry?: IdbPredictorEntry): Date {
+    let lastDate: Date;
+    if (monthlyData && predictorEntry) {
+      lastDate = _.max([new Date(monthlyData.date), new Date(predictorEntry.date)]);
+    } else if (monthlyData) {
+      lastDate = new Date(monthlyData.date);
+    } else if (predictorEntry) {
+      lastDate = new Date(predictorEntry.date);
+    }
+    return lastDate;
   }
 
 }
