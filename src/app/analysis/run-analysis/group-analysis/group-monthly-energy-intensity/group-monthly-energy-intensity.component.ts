c90e15973a5ec416ce30610b047f2648e0419865
rmroot <mroot@intertech.com>
2022-01-17 10:13:10 -0600

    add monthly energy intensity graph

--- src/app/analysis/run-analysis/group-analysis/group-monthly-energy-intensity/group-monthly-energy-intensity.component.ts
+++ src/app/analysis/run-analysis/group-analysis/group-monthly-energy-intensity/group-monthly-energy-intensity.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AnalysisService } from 'src/app/analysis/analysis.service';
-import { EnergyIntensityService, MonthlyGroupSummary } from 'src/app/analysis/calculations/energy-intensity.service';
+import { AnnualGroupSummary, EnergyIntensityService, MonthlyGroupSummary } from 'src/app/analysis/calculations/energy-intensity.service';
 import { AnalysisDbService } from 'src/app/indexedDB/analysis-db.service';
 import { FacilitydbService } from 'src/app/indexedDB/facility-db.service';
 import { AnalysisGroup, IdbAnalysisItem, IdbFacility } from 'src/app/models/idb';
@@ -16,14 +16,18 @@ export class GroupMonthlyEnergyIntensityComponent implements OnInit {
   analysisItem: IdbAnalysisItem;
   dataDisplay: 'table' | 'graph' = 'table';
   itemsPerPage: number = 6;
+  facility: IdbFacility;
+  baselineEnergyIntensity: number;
   constructor(private energyIntensityService: EnergyIntensityService, private analysisService: AnalysisService,
     private analysisDbService: AnalysisDbService, private facilityDbService: FacilitydbService) { }
 
   ngOnInit(): void {
     this.analysisItem = this.analysisDbService.selectedAnalysisItem.getValue();
     let group: AnalysisGroup = this.analysisService.selectedGroup.getValue();
-    let selectedFacility: IdbFacility = this.facilityDbService.selectedFacility.getValue();
-    this.monthlyGroupSummaries = this.energyIntensityService.calculateMonthlyGroupSummaries(this.analysisItem, group, selectedFacility);
+    this.facility = this.facilityDbService.selectedFacility.getValue();
+    this.monthlyGroupSummaries = this.energyIntensityService.calculateMonthlyGroupSummaries(this.analysisItem, group, this.facility);
+    let annualGroupSummaries: Array<AnnualGroupSummary> = this.energyIntensityService.calculateAnnualGroupSummaries(this.analysisItem, group, this.facility);    
+    this.baselineEnergyIntensity = annualGroupSummaries[0].energyIntensity;
   }
 
   setDataDisplay(display: 'table' | 'graph') {
--- src/app/analysis/run-analysis/group-analysis/group-monthly-energy-intensity/monthly-analysis-graph/monthly-analysis-graph.component.ts
+++ src/app/analysis/run-analysis/group-analysis/group-monthly-energy-intensity/monthly-analysis-graph/monthly-analysis-graph.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { PlotlyService } from 'angular-plotly.js';
+import { MonthlyGroupSummary } from 'src/app/analysis/calculations/energy-intensity.service';
+import { IdbAnalysisItem, IdbFacility } from 'src/app/models/idb';
+import { Month, Months } from 'src/app/form-data/months';
 
 @Component({
   selector: 'app-monthly-analysis-graph',
@@ -6,10 +10,121 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./monthly-analysis-graph.component.css']
 })
 export class MonthlyAnalysisGraphComponent implements OnInit {
+  @Input()
+  monthlyGroupSummaries: Array<MonthlyGroupSummary>;
+  @Input()
+  facility: IdbFacility;
+  @Input()
+  analysisItem: IdbAnalysisItem;
+  @Input()
+  baselineEnergyIntensity: number;
 
-  constructor() { }
+  @ViewChild('monthlyAnalysisGraph', { static: false }) monthlyAnalysisGraph: ElementRef;
+
+  yearData: Array<{
+    year: number,
+    summaries: Array<MonthlyGroupSummary>
+  }>
+  markerColors: Array<string> = [
+    'purple',
+    'blue',
+    'green',
+    'red',
+    'black',
+    'orange',
+    'yellow'
+  ]
+  constructor(private plotlyService: PlotlyService) { }
 
   ngOnInit(): void {
+    this.yearData = this.getYearData();
   }
 
+  ngAfterViewInit() {
+    this.drawChart();
+  }
+
+  drawChart() {
+    if (this.monthlyAnalysisGraph) {
+      let traceData = new Array();
+
+      let xData: Array<number> = this.yearData[0].summaries.map(summary => { return new Date(summary.date).getUTCMonth() });
+      let months: Array<string> = xData.map(data => { return this.getMonth(data) });
+      this.yearData.forEach((dataItem, index) => {
+        let yData: Array<number> = dataItem.summaries.map(summary => { return summary.energyIntensity });
+        traceData.push({
+          x: months,
+          y: yData,
+          mode: 'markers',
+          name: dataItem.year,
+          marker: {
+            color: this.markerColors[index],
+            size: 16
+          }
+        });
+
+      });
+
+
+      traceData.push({
+        x: months,
+        y: months.map(month => { return this.baselineEnergyIntensity }),
+        mode: 'lines',
+        name: 'Baseline EI',
+        marker: {
+          color: 'black'
+        }
+      });
+
+      let layout = {
+        title: 'Energy Intensity',
+        xaxis: {
+          title: 'Month',
+        },
+        yaxis: {
+          title: 'Energy Intensity',
+        }
+      };
+
+      var config = {
+        responsive: true,
+        modeBarButtonsToRemove: ['autoScale2d', 'lasso2d', 'pan2d', 'select2d', 'toggleSpikelines', 'hoverClosestCartesian', 'hoverCompareCartesian', 'autoscale', 'zoom', 'zoomin', 'zoomout'],
+        displaylogo: false,
+      };
+      this.plotlyService.newPlot(this.monthlyAnalysisGraph.nativeElement, traceData, layout, config);
+
+    }
+  }
+
+  getMonth(num: number): string {
+    let month: Month = Months.find(month => { return month.monthNumValue == num });
+    return month.abbreviation;
+  }
+
+
+  getYearData(): Array<{
+    year: number,
+    summaries: Array<MonthlyGroupSummary>
+  }> {
+    let traceData: Array<{
+      year: number,
+      summaries: Array<MonthlyGroupSummary>
+    }> = new Array();
+    let startDate: Date = new Date(this.facility.sustainabilityQuestions.energyReductionBaselineYear, 0);
+    let endDate: Date = new Date(this.analysisItem.reportYear, 12);
+
+    while (startDate.getUTCFullYear() < endDate.getUTCFullYear()) {
+      let yearGroupSummaries: Array<MonthlyGroupSummary> = this.monthlyGroupSummaries.filter(summary => {
+        let summaryDate: Date = new Date(summary.date);
+        return summaryDate.getUTCFullYear() == startDate.getUTCFullYear();
+      });
+
+      traceData.push({
+        year: new Date(startDate).getUTCFullYear(),
+        summaries: yearGroupSummaries
+      });
+      startDate.setUTCFullYear(startDate.getUTCFullYear() + 1);
+    }
+    return traceData;
+  }
 }
