b8df1f9011dac10d600adc80a5704acf4400b630
rmroot <mroot@intertech.com>
2022-01-24 14:29:29 -0600

    update facility summary graph and table

--- src/app/analysis/run-analysis/facility-analysis/facility-analysis.component.ts
+++ src/app/analysis/run-analysis/facility-analysis/facility-analysis.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AnalysisDbService } from 'src/app/indexedDB/analysis-db.service';
 import { FacilitydbService } from 'src/app/indexedDB/facility-db.service';
-import { FacilityGroupSummary } from 'src/app/models/analysis';
+import { FacilityGroupSummary, FacilityGroupTotals } from 'src/app/models/analysis';
 import { IdbAnalysisItem, IdbFacility } from 'src/app/models/idb';
 import { AnalysisService } from '../../analysis.service';
 import { EnergyIntensityService } from '../../calculations/energy-intensity.service';
@@ -18,6 +18,7 @@ export class FacilityAnalysisComponent implements OnInit {
   analysisItem: IdbAnalysisItem;
   facility: IdbFacility;
   facilityGroupSummaries: Array<FacilityGroupSummary>;
+  facilityGroupTotals: Array<FacilityGroupTotals>;
   constructor(private analysisService: AnalysisService, private energyIntensityService: EnergyIntensityService,
     private facilityDbService: FacilitydbService, private analysisDbService: AnalysisDbService) { }
 
@@ -26,7 +27,7 @@ export class FacilityAnalysisComponent implements OnInit {
     this.analysisItem = this.analysisDbService.selectedAnalysisItem.getValue();
     this.facility = this.facilityDbService.selectedFacility.getValue();
     this.facilityGroupSummaries = this.energyIntensityService.calculateFacilitySummary(this.analysisItem, this.facility);
-
+    this.facilityGroupTotals = this.energyIntensityService.calculateFacilityGroupTotals(this.facilityGroupSummaries, this.facility, this.analysisItem);
   }
 
   setDataDisplay(display: 'table' | 'graph') {
--- src/app/models/analysis.ts
+++ src/app/models/analysis.ts
@@ -1,56 +1,61 @@
 import { AnalysisGroup } from "./idb";
 
 export interface AnnualGroupSummary {
-    year: number,
-    totalEnergy: number,
-    totalEnergySavings: number,
-    annualEnergySavings: number,
-    totalProduction: number,
-    annualProductionChange: number,
-    totalProductionChange: number,
-    energyIntensity: number,
-    totalEnergyIntensityChange: number,
-    annualEnergyIntensityChange: number,
-    group: AnalysisGroup
-  }
-  
-  
-  export interface MonthlyGroupSummary {
-    date: Date,
-    energyUse: number,
-    production: number,
-    energyIntensity: number,
-    fiscalYear: number
-  }
-  
-  
-  export interface FacilityYearGroupSummary {
-    year: number,
-    group: AnalysisGroup,
-    percentBaseline: number,
-    energyIntensity: number,
-    annualEnergyIntensityChange: number,
-    energyIntensityImprovement: number,
-    improvementContribution: number,
-    totalEnergySavings: number,
-    annualEnergySavings: number,
-    totalEnergy: number,
-    totalProduction: number
-  }
-  
-  export interface FacilityGroupSummary {
-    yearGroupSummaries: Array<FacilityYearGroupSummary>
-    totals: {
-      year: number,
-      improvementContribution: number,
-      totalSavings: number,
-      newSavings: number,
-      energyIntensity: number,
-      annualEnergyIntensityChange: number,
-      energyIntensityImprovement: number,
-      totalEnergy: number,
-      totalProduction: number,
-      totalEnergySavings: number,
-      annualEnergySavings: number
-    }
-  }
+  year: number,
+  totalEnergy: number,
+  totalEnergySavings: number,
+  annualEnergySavings: number,
+  totalProduction: number,
+  annualProductionChange: number,
+  totalProductionChange: number,
+  energyIntensity: number,
+  totalEnergyIntensityChange: number,
+  annualEnergyIntensityChange: number,
+  group: AnalysisGroup
+}
+
+
+export interface MonthlyGroupSummary {
+  date: Date,
+  energyUse: number,
+  production: number,
+  energyIntensity: number,
+  fiscalYear: number
+}
+
+
+export interface FacilityYearGroupSummary {
+  year: number,
+  // group: AnalysisGroup,
+  // percentBaseline: number,
+  energyIntensity: number,
+  annualEnergyIntensityChange: number,
+  energyIntensityImprovement: number,
+  annualImprovementContribution: number,
+  totalImprovementContribution: number,
+  totalEnergySavings: number,
+  annualEnergySavings: number,
+  totalEnergy: number,
+  totalProduction: number
+}
+
+export interface FacilityGroupSummary {
+  group: AnalysisGroup,
+  collapsed: boolean,
+  percentBaseline: number,
+  summaries: Array<FacilityYearGroupSummary>,
+}
+
+export interface FacilityGroupTotals {
+  year: number,
+  // improvementContribution: number,
+  totalSavings: number,
+  newSavings: number,
+  energyIntensity: number,
+  annualEnergyIntensityChange: number,
+  energyIntensityImprovement: number,
+  totalEnergy: number,
+  // totalProduction: number,
+  totalEnergySavings: number,
+  annualEnergySavings: number
+}
