e0cbb6afd9a4e43d69e43f45e6dd9be89045da53
rmroot <mroot@intertech.com>
2022-02-16 14:07:59 -0600

    updates to account banner

--- src/app/account/account-banner/account-banner.component.ts
+++ src/app/account/account-banner/account-banner.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { HelpPanelService } from 'src/app/help-panel/help-panel.service';
+import { AccountdbService } from 'src/app/indexedDB/account-db.service';
+import { IdbAccount } from 'src/app/models/idb';
 
 @Component({
   selector: 'app-account-banner',
@@ -8,13 +12,43 @@ import { HelpPanelService } from 'src/app/help-panel/help-panel.service';
 })
 export class AccountBannerComponent implements OnInit {
 
-  constructor(private helpPanelService: HelpPanelService) { }
+
+  label: string;
+
+  selectedAccount: IdbAccount;
+  selectedAccountSub: Subscription;
+  constructor(private helpPanelService: HelpPanelService, private accountDbService: AccountdbService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.selectedAccountSub = this.accountDbService.selectedAccount.subscribe(account => {
+      this.selectedAccount = account;
+    });
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.setLabel(this.router.url);
+      }
+    });
+    this.setLabel(this.router.url);
   }
 
-  toggleHelpPanel(){
+  ngOnDestroy() {
+    this.selectedAccountSub.unsubscribe();
+  }
+
+  toggleHelpPanel() {
     let helpPanelOpen: boolean = this.helpPanelService.helpPanelOpen.getValue();
     this.helpPanelService.helpPanelOpen.next(!helpPanelOpen);
+  }
+
+
+  setLabel(url: string) {
+    if (this.selectedAccount) {
+      if (url.includes('settings')) {
+        this.label = this.selectedAccount.name + ' Settings'
+      } else if (url.includes('home')) {
+        this.label = this.selectedAccount.name + ' Overview'
+      }
+    }
   }
 }
