ad71903eb66d32f9f9cff2e85a3eda12c6a57c88
rmroot <mroot@intertech.com>
2022-02-17 12:01:15 -0600

    add icons and sidebar logic

--- src/app/core-components/sidebar/sidebar.component.ts
+++ src/app/core-components/sidebar/sidebar.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, Event } from '@angular/router';
 import { LocalStorageService } from 'ngx-webstorage';
+import { Subscription } from 'rxjs';
+import { AccountdbService } from 'src/app/indexedDB/account-db.service';
+import { FacilitydbService } from 'src/app/indexedDB/facility-db.service';
+import { IdbAccount, IdbFacility } from 'src/app/models/idb';
 import { environment } from 'src/environments/environment';
 
 @Component({
@@ -11,9 +15,19 @@ import { environment } from 'src/environments/environment';
 export class SidebarComponent implements OnInit {
   open: boolean = true;
   isDev: boolean;
-  isHovering: any;
-  openFromHover: boolean;
-  constructor(private localStorageService: LocalStorageService) {
+
+  account: IdbAccount;
+  accountName: string;
+  accountSub: Subscription;
+
+  facilityList: Array<IdbFacility>;
+  facilityListSub: Subscription;
+
+  selectedFacility: IdbFacility;
+  selectedFacilitySub: Subscription;
+
+  constructor(private localStorageService: LocalStorageService, private accountDbService: AccountdbService,
+    private facilityDbService: FacilitydbService, private router: Router) {
     let sidebarOpen: boolean = this.localStorageService.retrieve("sidebarOpen");
     if (sidebarOpen != undefined) {
       this.open = sidebarOpen;
@@ -22,18 +36,63 @@ export class SidebarComponent implements OnInit {
 
   ngOnInit() {
     this.isDev = !environment.production;
+    this.accountSub = this.accountDbService.selectedAccount.subscribe(val => {
+      this.account = val;
+      if (this.account) {
+        this.accountName = this.account.name;
+      }
+    });
+
+    this.facilityListSub = this.facilityDbService.accountFacilities.subscribe(val => {
+      this.facilityList = val;
+    });
+
+    this.selectedFacilitySub = this.facilityDbService.selectedFacility.subscribe(val => {
+      this.selectedFacility = val;
+    })
   }
 
-  toggleSidebar(fromClick: boolean) {
-    if (fromClick) {
-      this.openFromHover = false;
-    }
-    if (this.isHovering) {
-      clearTimeout(this.isHovering);
-    }
+  ngOnDestroy() {
+    this.accountSub.unsubscribe();
+    this.facilityListSub.unsubscribe();
+    this.selectedFacilitySub.unsubscribe();
+  }
+
+  toggleSidebar() {
     this.open = !this.open;
     window.dispatchEvent(new Event("resize"));
     this.localStorageService.store('sidebarOpen', this.open);
+  }
+
+  checkHideFacility(facility: IdbFacility): boolean {
+    if (this.open) {
+      return false;
+    } else if (this.router.url.includes('account')) {
+      return false;
+    } else if (this.selectedFacility) {
+      if (this.selectedFacility == facility) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  checkHideFacilityLinks(): boolean {
+    if (this.open) {
+      return false;
+    } else if (this.router.url.includes('account')) {
+      return true;
+    }
+    return false;
+  }
+
+  checkHideAccountLinks(): boolean {
+    if (this.open) {
+      return false;
+    } else if (!this.router.url.includes('account')) {
+      return true;
+    }
+    return false;
   }
 
 }
