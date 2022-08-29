8f1e897a2448d5aff7136a0554aa04fdef8b1063
rmroot <mroot@intertech.com>
2022-08-29 13:57:47 -0500

    map facility predictors setup

--- src/app/upload-data/data-setup/set-facility-predictors/set-facility-predictors.component.ts
+++ src/app/upload-data/data-setup/set-facility-predictors/set-facility-predictors.component.ts
@@ -1,4 +1,9 @@
+import { CdkDragDrop } from '@angular/cdk/drag-drop';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FacilitydbService } from 'src/app/indexedDB/facility-db.service';
+import { IdbFacility } from 'src/app/models/idb';
+import { ColumnItem, FacilityGroup, UploadDataService } from '../../upload-data.service';
 
 @Component({
   selector: 'app-set-facility-predictors',
@@ -7,9 +12,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SetFacilityPredictorsComponent implements OnInit {
 
-  constructor() { }
+  facilityGroups: Array<FacilityGroup>;
+  facilityGroupIds: Array<string>;
+  constructor(private uploadDataService: UploadDataService, private facilityDbService: FacilitydbService, private router: Router) { }
 
   ngOnInit(): void {
+    this.setFacilityGroups();
+    this.facilityGroupIds = this.facilityGroups.map(group => { return group.facilityId });
   }
 
+
+  setFacilityGroups() {
+    let facilityGroups: Array<FacilityGroup> = new Array();
+    let meterIndex: number = 0;
+    let unmappedPredictors: Array<ColumnItem> = new Array();
+    this.uploadDataService.fileReferences.forEach(ref => {
+      ref.columnGroups.forEach(group => {
+        if (group.groupLabel == 'Predictors') {
+          group.groupItems.forEach(item => {
+            unmappedPredictors.push({
+              index: meterIndex,
+              value: item.value,
+              id: item.id,
+              fileName: ref.name
+            });
+            meterIndex++;
+          })
+        }
+      });
+    });
+    facilityGroups.push({
+      facilityId: Math.random().toString(36).substr(2, 9),
+      groupItems: unmappedPredictors,
+      facilityName: 'Unmapped Predictors',
+      color: ''
+    })
+    let idbFacilities: Array<IdbFacility> = this.facilityDbService.accountFacilities.getValue();
+    idbFacilities.forEach(facility => {
+      facilityGroups.push({
+        facilityId: facility.guid,
+        groupItems: [],
+        facilityName: facility.name,
+        color: facility.color
+      });
+    });
+    this.facilityGroups = facilityGroups;
+  }
+
+  dropColumn(dropData: CdkDragDrop<string[]>) {
+    this.facilityGroups.forEach(group => {
+      if (group.facilityId == dropData.previousContainer.id) {
+        //remove
+        let itemIndex: number = group.groupItems.findIndex(groupItem => { return groupItem.id == dropData.item.data.id });
+        if (itemIndex > -1) {
+          group.groupItems.splice(itemIndex, 1);
+        }
+      }
+      if (group.facilityId == dropData.container.id) {
+        //add
+        group.groupItems.push(dropData.item.data);
+      }
+    });
+  }
+
+  continue() {
+    // this.router.navigateByUrl('/upload/data-setup/set-facility-predictors');
+  }
 }
