417291f63893aad090b5771d801f821100e57603
rmroot <mroot@intertech.com>
2022-09-22 14:26:11 -0500

    update facility list in sidebar to use ordering

--- src/app/core-components/sidebar/facility-list.pipe.ts
+++ src/app/core-components/sidebar/facility-list.pipe.ts
@@ -11,12 +11,8 @@ export class FacilityListPipe implements PipeTransform {
   transform(facilityList: Array<FacilityListItem>, showAll?: boolean): Array<FacilityListItem> {
     if (facilityList.length > 4) {
       let orderedList: Array<FacilityListItem> = _.orderBy(facilityList, (facilityListItem: FacilityListItem) => {
-        if (facilityListItem.modifiedDate) {
-          return new Date(facilityListItem.modifiedDate)
-        } else {
-          return undefined;
-        }
-      }, ['desc']);
+          return facilityListItem.facilityOrder;
+      }, ['asc']);
       if (!showAll) {
         return orderedList.splice(0, 4)
       } else {
