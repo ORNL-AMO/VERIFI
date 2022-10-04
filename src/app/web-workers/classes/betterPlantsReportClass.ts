28b52fca3e7864863dcdbd6b0f84a004c0f903e1
rmroot <mroot@intertech.com>
2022-10-04 11:27:06 -0500

    start better plants web worker

---
+++ src/app/web-workers/classes/betterPlantsReportClass.ts
@@ -0,0 +1,9 @@
+import { CalanderizedMeter } from "src/app/models/calanderization";
+import { IdbAccountAnalysisItem } from "src/app/models/idb";
+
+export class BetterPlantsReportClass {
+
+    constructor(selectedAnalysisItem: IdbAccountAnalysisItem, calanderizedMeters: Array<CalanderizedMeter>,){
+
+    }
+}
