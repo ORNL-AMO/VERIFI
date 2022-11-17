ea58a866a630e48ff9b8b2599780118f108f5818
rmroot <mroot@intertech.com>
2022-11-17 16:00:13 -0600

    fix saving in analysis setup screens

--- src/app/facility/analysis/run-analysis/analysis-setup/analysis-setup.component.ts
+++ src/app/facility/analysis/run-analysis/analysis-setup/analysis-setup.component.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { Month, Months } from 'src/app/shared/form-data/months';
 import { AnalysisDbService } from 'src/app/indexedDB/analysis-db.service';
 import { FacilitydbService } from 'src/app/indexedDB/facility-db.service';
-import { IdbAnalysisItem, IdbFacility } from 'src/app/models/idb';
+import { IdbAccount, IdbAnalysisItem, IdbFacility } from 'src/app/models/idb';
 import { EnergyUnitOptions, UnitOption } from 'src/app/shared/unitOptions';
 import * as _ from 'lodash';
 import { AnalysisCalculationsHelperService } from 'src/app/shared/shared-analysis/calculations/analysis-calculations-helper.service';
 import { AnalysisService } from '../../analysis.service';
 import { Router } from '@angular/router';
 import { AnalysisValidationService } from '../../analysis-validation.service';
+import { DbChangesService } from 'src/app/indexedDB/db-changes.service';
+import { AccountdbService } from 'src/app/indexedDB/account-db.service';
 @Component({
   selector: 'app-analysis-setup',
   templateUrl: './analysis-setup.component.html',
@@ -26,7 +28,9 @@ export class AnalysisSetupComponent implements OnInit {
   constructor(private facilityDbService: FacilitydbService, private analysisDbService: AnalysisDbService,
     private analysisCalculationsHelperService: AnalysisCalculationsHelperService,
     private analysisService: AnalysisService, private router: Router,
-    private analysisValidationService: AnalysisValidationService) { }
+    private analysisValidationService: AnalysisValidationService,
+    private dbChangesService: DbChangesService,
+    private accountDbService: AccountdbService) { }
 
   ngOnInit(): void {
     this.analysisItem = this.analysisDbService.selectedAnalysisItem.getValue();
@@ -38,6 +42,9 @@ export class AnalysisSetupComponent implements OnInit {
   async saveItem() {
     this.analysisItem.setupErrors = this.analysisValidationService.getAnalysisItemErrors(this.analysisItem);
     await this.analysisDbService.updateWithObservable(this.analysisItem).toPromise();
+    let selectedAccount: IdbAccount = this.accountDbService.selectedAccount.getValue();
+    let selectedFacility: IdbFacility = this.facilityDbService.selectedFacility.getValue();
+    await this.dbChangesService.setAnalysisItems(selectedAccount, selectedFacility);
     this.analysisDbService.selectedAnalysisItem.next(this.analysisItem);
   }
 
