8ac3f2d19d45d481902ac6b5ce4c62d4301ede72
rmroot <mroot@intertech.com>
2022-12-14 12:49:59 -0600

    cleanup unused account analysis dashboard code

--- src/app/account/account-analysis/account-analysis-dashboard/account-analysis-dashboard.component.ts
+++ src/app/account/account-analysis/account-analysis-dashboard/account-analysis-dashboard.component.ts
@@ -7,7 +7,6 @@ import { AccountdbService } from 'src/app/indexedDB/account-db.service';
 import { DbChangesService } from 'src/app/indexedDB/db-changes.service';
 import { UtilityMeterDatadbService } from 'src/app/indexedDB/utilityMeterData-db.service';
 import { IdbAccount, IdbAccountAnalysisItem } from 'src/app/models/idb';
-import { SharedDataService } from 'src/app/shared/helper-services/shared-data.service';
 import * as _ from 'lodash';
 import { AnalysisService } from 'src/app/facility/analysis/analysis.service';
 
@@ -18,16 +17,8 @@ import { AnalysisService } from 'src/app/facility/analysis/analysis.service';
 })
 export class AccountAnalysisDashboardComponent implements OnInit {
 
-  accountAnalysisItems: Array<IdbAccountAnalysisItem>;
   accountAnalysisItemsSub: Subscription;
 
-  currentPageNumber: number = 1;
-  itemsPerPage: number;
-  itemsPerPageSub: Subscription;
-  orderDataField: string = 'name';
-  orderByDirection: string = 'desc';
-
-  itemToDelete: IdbAccountAnalysisItem;
   baselineYearError: boolean;
   yearOptions: Array<number>;
   selectedAccount: IdbAccount;
@@ -40,12 +31,11 @@ export class AccountAnalysisDashboardComponent implements OnInit {
   showDetailSub: Subscription;
   constructor(private router: Router, private accountAnalysisDbService: AccountAnalysisDbService, private toastNotificationService: ToastNotificationsService,
     private accountDbService: AccountdbService, private utilityMeterDataDbService: UtilityMeterDatadbService,
-    private dbChangesService: DbChangesService, private sharedDataService: SharedDataService, private analysisService: AnalysisService) { }
+    private dbChangesService: DbChangesService, private analysisService: AnalysisService) { }
 
   ngOnInit(): void {
     this.selectedAccount = this.accountDbService.selectedAccount.getValue();
     this.accountAnalysisItemsSub = this.accountAnalysisDbService.accountAnalysisItems.subscribe(items => {
-      // this.accountAnalysisItems = items;
       this.setAnalysisItemsList(items);
     });
 
@@ -53,11 +43,7 @@ export class AccountAnalysisDashboardComponent implements OnInit {
     if (this.yearOptions) {
       this.baselineYearError = this.yearOptions[0] > this.selectedAccount.sustainabilityQuestions.energyReductionBaselineYear
     }
-
-    this.itemsPerPageSub = this.sharedDataService.itemsPerPage.subscribe(val => {
-      this.itemsPerPage = val;
-    })
-    
+   
     this.showDetailSub = this.analysisService.showDetail.subscribe(showDetail => {
       this.showDetail = showDetail;
     })
@@ -65,7 +51,6 @@ export class AccountAnalysisDashboardComponent implements OnInit {
 
   ngOnDestroy() {
     this.accountAnalysisItemsSub.unsubscribe();
-    this.itemsPerPageSub.unsubscribe();
     this.showDetailSub.unsubscribe();
   }
 
@@ -77,56 +62,7 @@ export class AccountAnalysisDashboardComponent implements OnInit {
     this.toastNotificationService.showToast('Analysis Item Created', undefined, undefined, false, "success");
     this.router.navigateByUrl('account/analysis/setup');
   }
-
-  selectAnalysisItem(item: IdbAccountAnalysisItem) {
-    this.accountAnalysisDbService.selectedAnalysisItem.next(item);
-    //todo: route to results if item setup
-    this.router.navigateByUrl('account/analysis/setup');
-  }
-
-  deleteItem(item: IdbAccountAnalysisItem) {
-    this.itemToDelete = item;
-  }
-
-  cancelDelete() {
-    this.itemToDelete = undefined;
-  }
-
-  async confirmDelete() {
-    await this.accountAnalysisDbService.deleteWithObservable(this.itemToDelete.id).toPromise();
-    await this.dbChangesService.setAccountAnalysisItems(this.selectedAccount);
-    this.itemToDelete = undefined;
-    this.toastNotificationService.showToast('Analysis Item Deleted', undefined, undefined, false, "success");
-  }
-
-  editItem(item: IdbAccountAnalysisItem) {
-    this.accountAnalysisDbService.selectedAnalysisItem.next(item);
-    this.router.navigateByUrl('account/analysis/setup');
-  }
-
-  setOrderDataField(str: string) {
-    if (str == this.orderDataField) {
-      if (this.orderByDirection == 'desc') {
-        this.orderByDirection = 'asc';
-      } else {
-        this.orderByDirection = 'desc';
-      }
-    } else {
-      this.orderDataField = str;
-    }
-  }
-
-  async createCopy(analysisItem: IdbAccountAnalysisItem) {
-    let newItem: IdbAccountAnalysisItem = JSON.parse(JSON.stringify(analysisItem));
-    delete newItem.id;
-    newItem.name = newItem.name + ' (Copy)';
-    let addedItem: IdbAccountAnalysisItem = await this.accountAnalysisDbService.addWithObservable(newItem).toPromise();
-    await this.dbChangesService.setAccountAnalysisItems(this.selectedAccount);
-    this.accountAnalysisDbService.selectedAnalysisItem.next(addedItem);
-    this.toastNotificationService.showToast('Analysis Item Copy Created', undefined, undefined, false, "success");
-    this.router.navigateByUrl('account/analysis/setup');
-
-  }
+  
 
   setAnalysisItemsList(accountAnalysisItems: Array<IdbAccountAnalysisItem>) {
     this.analysisItemsList = new Array();
@@ -146,6 +82,4 @@ export class AccountAnalysisDashboardComponent implements OnInit {
   saveShowDetails() {
     this.analysisService.showDetail.next(this.showDetail);
   }
-
-
 }
