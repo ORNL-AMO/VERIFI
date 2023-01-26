8e97abc3e1553316ef7eefc653388eb3473dbb36
rmroot <mroot@intertech.com>
2023-01-26 11:15:13 -0600

    add create report from analysis

--- src/app/core-components/create-report-modal/create-report-modal.component.ts
+++ src/app/core-components/create-report-modal/create-report-modal.component.ts
@@ -4,11 +4,12 @@ import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
 import { AccountReportDbService } from 'src/app/indexedDB/account-report-db.service';
 import { DbChangesService } from 'src/app/indexedDB/db-changes.service';
-import { IdbAccount, IdbAccountReport } from 'src/app/models/idb';
+import { IdbAccount, IdbAccountAnalysisItem, IdbAccountReport } from 'src/app/models/idb';
 import { AccountdbService } from 'src/app/indexedDB/account-db.service';
 import { AccountOverviewService } from 'src/app/account/account-overview/account-overview.service';
 import { ToastNotificationsService } from '../toast-notifications/toast-notifications.service';
 import { UtilityMeterDatadbService } from 'src/app/indexedDB/utilityMeterData-db.service';
+import { AccountAnalysisDbService } from 'src/app/indexedDB/account-analysis-db.service';
 
 @Component({
   selector: 'app-create-report-modal',
@@ -25,7 +26,8 @@ export class CreateReportModalComponent {
     private accountDbService: AccountdbService,
     private accountOverviewService: AccountOverviewService,
     private toastNotificationService: ToastNotificationsService,
-    private utilityMeterDataDbService: UtilityMeterDatadbService) {
+    private utilityMeterDataDbService: UtilityMeterDatadbService,
+    private accountAnalysisDbService: AccountAnalysisDbService) {
 
   }
 
@@ -42,39 +44,49 @@ export class CreateReportModalComponent {
   async createReport() {
     let newReport: IdbAccountReport = this.accountReportDbService.getNewAccountReport();
     let account: IdbAccount = this.accountDbService.selectedAccount.getValue();
+    let navigateToStr: string;
     if (this.router.url.includes('account/overview')) {
+      navigateToStr = 'account/account-reports/data-overview-report';
       newReport.reportType = 'dataOverview';
       let yearOptions: Array<number> = this.utilityMeterDataDbService.getYearOptions(true);
       newReport.baselineYear = yearOptions[0];
       newReport.reportYear = yearOptions[yearOptions.length - 1];
       if (this.router.url.includes('energy')) {
-        newReport.name = account.name + ' Energy Report';
+        newReport.name = 'Energy Report';
         newReport.dataOverviewReportSetup.accountCostsSection.includeSection = false;
         newReport.dataOverviewReportSetup.accountEmissionsSection.includeSection = false;
         newReport.dataOverviewReportSetup.accountWaterSection.includeSection = false;
       } else if (this.router.url.includes('costs')) {
-        newReport.name = account.name + ' Costs Report';
+        newReport.name = 'Costs Report';
         newReport.dataOverviewReportSetup.accountEmissionsSection.includeSection = false;
         newReport.dataOverviewReportSetup.accountEnergySection.includeSection = false;
         newReport.dataOverviewReportSetup.accountWaterSection.includeSection = false;
       } else if (this.router.url.includes('emissions')) {
-        newReport.name = account.name + ' Emissions Report';
+        newReport.name = 'Emissions Report';
         newReport.dataOverviewReportSetup.accountCostsSection.includeSection = false;
         newReport.dataOverviewReportSetup.accountEnergySection.includeSection = false;
         newReport.dataOverviewReportSetup.accountWaterSection.includeSection = false;
         newReport.dataOverviewReportSetup.emissionsDisplay = this.accountOverviewService.emissionsDisplay.getValue();
       } else if (this.router.url.includes('water')) {
-        newReport.name = account.name + ' Water Report';
+        newReport.name = 'Water Report';
         newReport.dataOverviewReportSetup.accountCostsSection.includeSection = false;
         newReport.dataOverviewReportSetup.accountEnergySection.includeSection = false;
         newReport.dataOverviewReportSetup.accountEmissionsSection.includeSection = false;
       }
+    } else if (this.router.url.includes('account/analysis')) {
+      let selectedAnalysisItem: IdbAccountAnalysisItem = this.accountAnalysisDbService.selectedAnalysisItem.getValue();
+      newReport.name = 'Better Plants Report';
+      newReport.reportType = 'betterPlants';
+      newReport.baselineYear = account.sustainabilityQuestions.energyReductionBaselineYear;
+      newReport.reportYear = selectedAnalysisItem.reportYear;
+      newReport.betterPlantsReportSetup.analysisItemId = selectedAnalysisItem.guid;
+      navigateToStr = 'account/account-reports/better-plants-report';
     }
     let addedReport: IdbAccountReport = await this.accountReportDbService.addWithObservable(newReport).toPromise();
     await this.dbChangesService.setAccountReports(account);
     this.accountReportDbService.selectedReport.next(addedReport);
     this.toastNotificationService.showToast('Report Created', undefined, undefined, false, "bg-success");
     this.sharedDataService.openCreateReportModal.next(false);
-    this.router.navigateByUrl('account/account-reports/data-overview-report');
+    this.router.navigateByUrl(navigateToStr);
   }
 }
