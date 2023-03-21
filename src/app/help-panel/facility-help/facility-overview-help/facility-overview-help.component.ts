87e85386eb1dc8b9090445b89171d1a584f6f4ef
rmroot <mroot@intertech.com>
2023-03-21 14:44:41 -0500

    facility overview help text

--- src/app/help-panel/facility-help/facility-overview-help/facility-overview-help.component.ts
+++ src/app/help-panel/facility-help/facility-overview-help/facility-overview-help.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FacilitydbService } from 'src/app/indexedDB/facility-db.service';
 import { IdbFacility } from 'src/app/models/idb';
 import { Subscription } from 'rxjs';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-facility-overview-help',
@@ -12,15 +13,43 @@ export class FacilityOverviewHelpComponent {
 
   selectedFacility: IdbFacility
   selectedFacilitySub: Subscription;
-  constructor(private facilityDbService: FacilitydbService) { }
+  routerSub: Subscription;
+  overviewType: 'an energy consumption' | 'a utility cost' | 'a water consumption' | 'an emissions';
+  tableType: 'Utility Costs' | 'Utility Use and Cost' | 'Utility Emissions';
+  constructor(private facilityDbService: FacilitydbService,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.selectedFacilitySub = this.facilityDbService.selectedFacility.subscribe(selectedFacility => {
       this.selectedFacility = selectedFacility;
     });
+    this.routerSub = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        this.setOverviewType(event.urlAfterRedirects);
+      }
+    });
+    //navigationsEnd isn't fired on init. Call here.
+    this.setOverviewType(this.router.url);
   }
 
-  ngOnDestroy(){
+  ngOnDestroy() {
     this.selectedFacilitySub.unsubscribe();
+  }
+
+
+  setOverviewType(urlStr: string) {
+    if (urlStr.includes('energy')) {
+      this.overviewType = 'an energy consumption';
+      this.tableType = 'Utility Use and Cost';
+    } else if (urlStr.includes('water')) {
+      this.overviewType = 'a water consumption';
+      this.tableType = 'Utility Use and Cost';
+    } else if (urlStr.includes('costs')) {
+      this.overviewType = 'a utility cost'
+      this.tableType = 'Utility Costs';
+    } else if (urlStr.includes('emissions')) {
+      this.overviewType = 'an emissions';
+      this.tableType = 'Utility Emissions';
+    }
   }
 }
