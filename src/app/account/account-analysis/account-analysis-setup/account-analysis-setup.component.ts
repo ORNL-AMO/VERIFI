cad23777815900a1ec0cf4b83642c0fffd046b63
rmroot <mroot@intertech.com>
2023-03-27 10:35:52 -0500

    will set selected analysis item by default

--- src/app/account/account-analysis/account-analysis-setup/account-analysis-setup.component.ts
+++ src/app/account/account-analysis/account-analysis-setup/account-analysis-setup.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AccountAnalysisDbService } from 'src/app/indexedDB/account-analysis-db.service';
 import { AccountdbService } from 'src/app/indexedDB/account-db.service';
-import { IdbAccount, IdbAccountAnalysisItem } from 'src/app/models/idb';
+import { IdbAccount, IdbAccountAnalysisItem, IdbAnalysisItem } from 'src/app/models/idb';
 import { Month, Months } from 'src/app/shared/form-data/months';
 import { EnergyUnitOptions, UnitOption } from 'src/app/shared/unitOptions';
 import { AccountAnalysisService } from '../account-analysis.service';
 import { DbChangesService } from 'src/app/indexedDB/db-changes.service';
 import { UtilityMeterDatadbService } from 'src/app/indexedDB/utilityMeterData-db.service';
+import { AnalysisDbService } from 'src/app/indexedDB/analysis-db.service';
 
 @Component({
   selector: 'app-account-analysis-setup',
@@ -26,7 +27,8 @@ export class AccountAnalysisSetupComponent implements OnInit {
   constructor(private accountDbService: AccountdbService, private accountAnalysisDbService: AccountAnalysisDbService,
     private utilityMeterDataDbService: UtilityMeterDatadbService,
     private router: Router, private accountAnalysisService: AccountAnalysisService,
-    private dbChangesService: DbChangesService) { }
+    private dbChangesService: DbChangesService,
+    private analysisDbService: AnalysisDbService) { }
 
   ngOnInit(): void {
     this.analysisItem = this.accountAnalysisDbService.selectedAnalysisItem.getValue();
@@ -45,7 +47,7 @@ export class AccountAnalysisSetupComponent implements OnInit {
     this.accountAnalysisDbService.selectedAnalysisItem.next(this.analysisItem);
   }
 
-  changeReportYear() {
+  async changeReportYear() {
     if (this.account.sustainabilityQuestions.energyReductionBaselineYear < this.analysisItem.reportYear) {
       let yearAdjustments: Array<{ year: number, amount: number }> = new Array();
       for (let year: number = this.account.sustainabilityQuestions.energyReductionBaselineYear + 1; year <= this.analysisItem.reportYear; year++) {
@@ -56,18 +58,26 @@ export class AccountAnalysisSetupComponent implements OnInit {
       }
       this.analysisItem.baselineAdjustments = yearAdjustments;
     }
-    this.resetFacilityItems();
+    await this.resetFacilityItems();
   }
 
-  resetFacilityItems() {
+  async resetFacilityItems() {
+    let accountAnalysisItems: Array<IdbAnalysisItem> = this.analysisDbService.accountAnalysisItems.getValue();
     this.analysisItem.facilityAnalysisItems.forEach(item => {
-      item.analysisItemId = undefined;
+      let facilityItem: IdbAnalysisItem = accountAnalysisItems.find(accountItem => {
+        return accountItem.reportYear == this.analysisItem.reportYear && accountItem.facilityId == item.facilityId && accountItem.selectedYearAnalysis;
+      });
+      if (facilityItem) {
+        item.analysisItemId = facilityItem.guid;
+      } else {
+        item.analysisItemId = undefined;
+      }
     });
-    this.saveItem();
+    await this.saveItem();
   }
 
-  changeSiteSource() {
-    this.resetFacilityItems();
+  async changeSiteSource() {
+    await this.resetFacilityItems();
     this.accountAnalysisService.setCalanderizedMeters();
   }
 
