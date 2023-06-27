a715f93d3d6fc0128041b6e945270042e17dd0ed
rmroot <mroot@intertech.com>
2023-06-27 13:48:58 -0500

    start enhanced error page

--- src/app/core-components/startup-error/startup-error.component.ts
+++ src/app/core-components/startup-error/startup-error.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { AccountdbService } from 'src/app/indexedDB/account-db.service';
 import { LoadingService } from '../loading/loading.service';
+import { IdbAccount } from 'src/app/models/idb';
 
 @Component({
   selector: 'app-startup-error',
@@ -11,8 +12,13 @@ export class StartupErrorComponent {
   @Input()
   startupError: string;
 
+  accounts: Array<IdbAccount>;
   constructor(private accountdbService: AccountdbService, private loadingService: LoadingService){
 
+  }
+
+  ngOnInit(){
+    this.accounts = this.accountdbService.allAccounts.getValue();
   }
 
   resetDatabase(){
