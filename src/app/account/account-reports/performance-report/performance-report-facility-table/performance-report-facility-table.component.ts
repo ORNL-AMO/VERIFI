f1e52e7410dfda8482d76341043bc2fe001fac75
rmroot <mroot@intertech.com>
2023-08-01 13:49:06 -0500

    facility performance table working with ordering

--- src/app/account/account-reports/performance-report/performance-report-facility-table/performance-report-facility-table.component.ts
+++ src/app/account/account-reports/performance-report/performance-report-facility-table/performance-report-facility-table.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input } from '@angular/core';
-import { PerformanceReport } from 'src/app/calculations/performance-report-calculations/performanceReport';
-import { IdbAccount, IdbAccountAnalysisItem } from 'src/app/models/idb';
+import { PerformanceReport, PerformanceReportAnnualData } from 'src/app/calculations/performance-report-calculations/performanceReport';
+import { IdbAccount, IdbAccountAnalysisItem, IdbFacility } from 'src/app/models/idb';
+import * as _ from 'lodash';
+import { AnnualAnalysisSummary } from 'src/app/models/analysis';
 
 @Component({
   selector: 'app-performance-report-facility-table',
@@ -16,15 +18,54 @@ export class PerformanceReportFacilityTableComponent {
   selectedAnalysisItem: IdbAccountAnalysisItem;
 
   savingsGoal: number;
-  constructor(){
+  orderDataField: string = 'facilityName';
+  orderByYear: number;
+  orderByDirection: 'asc' | 'desc' = 'asc';
+  units: string;
+  constructor() {
 
   }
 
-  ngOnInit(){
-    if(this.selectedAnalysisItem.analysisCategory == 'energy'){
+  ngOnInit() {
+    if (this.selectedAnalysisItem.analysisCategory == 'energy') {
       this.savingsGoal = this.account.sustainabilityQuestions.energyReductionPercent;
-    }else if(this.selectedAnalysisItem.analysisCategory == 'water'){
+      this.units = this.selectedAnalysisItem.energyUnit;
+    } else if (this.selectedAnalysisItem.analysisCategory == 'water') {
       this.savingsGoal = this.account.sustainabilityQuestions.waterReductionPercent;
+      this.units = this.selectedAnalysisItem.waterUnit;
+    }
+    this.orderData();
+  }
+
+  setOrderDataField(str: string, year: number) {
+    this.orderByYear = year;
+    if (str == this.orderDataField) {
+      if (this.orderByDirection == 'desc') {
+        this.orderByDirection = 'asc';
+      } else {
+        this.orderByDirection = 'desc';
+      }
+    } else {
+      this.orderDataField = str;
+    }
+    this.orderData();
+  }
+
+  orderData() {
+    if (this.orderDataField == 'facilityName') {
+      this.performanceReport.annualFacilityData = _.orderBy(this.performanceReport.annualFacilityData, (data: { facility: IdbFacility, annualData: Array<PerformanceReportAnnualData> }) => {
+        return data.facility.name;
+      }, this.orderByDirection);
+    } else if (this.orderDataField == 'state') {
+      this.performanceReport.annualFacilityData = _.orderBy(this.performanceReport.annualFacilityData, (data: { facility: IdbFacility, annualData: Array<PerformanceReportAnnualData> }) => {
+        return data.facility.state;
+      }, this.orderByDirection);
+    } else {
+      this.performanceReport.annualFacilityData = _.orderBy(this.performanceReport.annualFacilityData, (data: { facility: IdbFacility, annualData: Array<PerformanceReportAnnualData> }) => {
+        let yearSummary: PerformanceReportAnnualData = data.annualData.find(summary => { return summary.year == this.orderByYear })
+        return yearSummary[this.orderDataField];
+      }, this.orderByDirection);
     }
   }
+
 }
