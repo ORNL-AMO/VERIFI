8ba5881bd493ec2f651d41b22fea26c14e363df7
rmroot <rootrm@ornl.gov>
2023-09-01 10:07:13 -0500

    add type label to report cards for water or energy

---
+++ src/app/shared/helper-pipes/analysis-category.pipe.ts
@@ -0,0 +1,27 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { AccountAnalysisDbService } from 'src/app/indexedDB/account-analysis-db.service';
+import { IdbAccountAnalysisItem } from 'src/app/models/idb';
+
+@Pipe({
+  name: 'analysisCategory'
+})
+export class AnalysisCategoryPipe implements PipeTransform {
+
+  constructor(private accountAnalysisDbService: AccountAnalysisDbService) {
+  }
+
+  transform(analysisId: string): 'Energy' | 'Water' | 'No Item Found' {
+    if (analysisId) {
+      let accountAnalysisItem: IdbAccountAnalysisItem = this.accountAnalysisDbService.getAccountAnalysisItem(analysisId);
+      if (accountAnalysisItem) {
+        if(accountAnalysisItem.analysisCategory == 'energy'){
+          return 'Energy';
+        }else if(accountAnalysisItem.analysisCategory == 'water'){
+          return 'Water';
+        }
+      }
+    }
+    return 'No Item Found';
+  }
+
+}
