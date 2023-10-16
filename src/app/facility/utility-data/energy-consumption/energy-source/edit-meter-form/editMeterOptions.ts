ae64283116911e8d086594f9956023e5876aef46
rmroot <rootrm@ornl.gov>
2023-10-16 11:35:00 -0500

    add initial CO2, CH4, N2O values to fuel options

--- src/app/facility/utility-data/energy-consumption/energy-source/edit-meter-form/editMeterOptions.ts
+++ src/app/facility/utility-data/energy-consumption/energy-source/edit-meter-form/editMeterOptions.ts
@@ -8,6 +8,10 @@ export interface FuelTypeOption {
     siteToSourceMultiplier: number,
     emissionsOutputRate: number,
     otherEnergyType?: 'Hot Water' | 'Steam' | 'Compressed Air' | 'Chilled Water';
+    CO2: number,
+    CH4: number,
+    N2O: number,
+    isBiofuel?: boolean
 }
 
 export const SourceOptions: Array<MeterSource> = [
@@ -26,49 +30,73 @@ export const GasOptions: Array<FuelTypeOption> = [
         heatCapacityValue: .000092,
         value: 'Blast Furnace Gas',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 274.3504
+        emissionsOutputRate: 274.3504,
+        CO2: 274.32,
+        CH4: 0.022,
+        N2O: 0.10
     },
     {
         startingUnit: 'ft3',
         heatCapacityValue: .000599,
         value: 'Coke Oven Gas',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 46.8918
+        emissionsOutputRate: 46.8918,
+        CO2: 48.85,
+        CH4: .48,
+        N2O: .1,
     },
+    //CHECK VALUES LPG
     {
         startingUnit: 'ft3',
         heatCapacityValue: .0027,
         value: 'Liquefied Petroleum Gas (LPG)',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 61.9638
+        emissionsOutputRate: 61.9638,
+        CO2: 61.71,
+        CH4: 3,
+        N2O: .6,
     },
     {
         startingUnit: 'ft3',
         heatCapacityValue: .002516,
         value: 'Propane',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 61.7138
+        emissionsOutputRate: 61.7138,
+        CO2: 61.46,
+        CH4: 3,
+        N2O: .6,
     },
+    //CHECK VALUES Butane
     {
         startingUnit: 'ft3',
         heatCapacityValue: .00328,
         value: 'Butane',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 64.0238
+        emissionsOutputRate: 64.0238,
+        CO2: 64.77,
+        CH4: 3,
+        N2O: .6,
     },
+    //CHECK VALUES Isobutane
     {
         startingUnit: 'ft3',
         heatCapacityValue: .0031,
         value: 'Isobutane',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 64.94
+        emissionsOutputRate: 64.94,
+        CO2: 64.94,
+        CH4: 3,
+        N2O: .6,
     },
     {
         startingUnit: 'ft3',
         heatCapacityValue: .000485,
         value: 'Landfill Gas',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 52.33774
+        emissionsOutputRate: 52.33774,
+        CO2: 52.07,
+        CH4: 3.2,
+        N2O: .63,
     }
 ]
 
@@ -78,168 +106,252 @@ export const LiquidOptions: Array<FuelTypeOption> = [
         heatCapacityValue: .0916476,
         value: 'Propane',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 63.1238
+        emissionsOutputRate: 63.1238,
+        CO2: 62.87,
+        CH4: 3,
+        N2O: .6
     },
     {
         startingUnit: 'gal',
         heatCapacityValue: .092,
         value: 'LPG',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 61.9638
+        emissionsOutputRate: 61.9638,
+        CO2: 61.71,
+        CH4: 3,
+        N2O: .6
     },
+    //CHECK VALUES Distillate Fuel Oil (1,2,3?)
+    //using 1
     {
         startingUnit: 'gal',
         heatCapacityValue: .141,
         value: 'Distillate Fuel Oil',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 74.33713
+        emissionsOutputRate: 74.33713,
+        CO2: 73.25,
+        CH4: 3.0,
+        N2O: .6
     },
+    //CHECK VALUES: Residual Fuel Oil (No. 5, 6?)
+    //using 5
     {
         startingUnit: 'gal',
         heatCapacityValue: .145,
         value: 'Residual Fuel Oil',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 74.2688
+        emissionsOutputRate: 74.2688,
+        CO2: 72.93,
+        CH4: 3,
+        N2O: .6
     },
+    //DOESN'T Exist in table (Residual)
     {
         startingUnit: 'gal',
         heatCapacityValue: .13869,
         value: 'Heating Oil',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 74.33713
+        emissionsOutputRate: 74.33713,
+        CO2: 0,
+        CH4: 0,
+        N2O: 0
     },
+    //Fuel oil #2
     {
         startingUnit: 'gal',
         heatCapacityValue: .13869,
         value: 'Diesel',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 74.2138
+        emissionsOutputRate: 74.2138,
+        CO2: 73.96,
+        CH4: 3,
+        N2O: .6
     },
     {
         startingUnit: 'gal',
         heatCapacityValue: .102032,
         value: 'Butane',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 65.0238
+        emissionsOutputRate: 65.0238,
+        CO2: 64.77,
+        CH4: 3.0,
+        N2O: .6
     },
     {
         startingUnit: 'gal',
         heatCapacityValue: .099,
         value: 'Isobutane',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 65.1938
+        emissionsOutputRate: 65.1938,
+        CO2: 64.94,
+        CH4: .3,
+        N2O: .6
     },
     {
         startingUnit: 'gal',
         heatCapacityValue: .1108,
         value: 'Pentane',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 70.0738
+        emissionsOutputRate: 70.0738,
+        CO2: 70.02,
+        CH4: 3,
+        N2O: .6
     },
     {
         startingUnit: 'gal',
         heatCapacityValue: .058,
         value: 'Ethylene',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 66.2138
+        emissionsOutputRate: 66.2138,
+        CO2: 65.96,
+        CH4: 3.0,
+        N2O: .6
     },
     {
         startingUnit: 'gal',
         heatCapacityValue: .091,
         value: 'Propylene',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 68.0238
+        emissionsOutputRate: 68.0238,
+        CO2: 67.77,
+        CH4: 3.0,
+        N2O: .6
     },
+    //CHECK VALUES: All Fuel Oils #x HHV doesn't match table
     {
         startingUnit: 'gal',
         heatCapacityValue: .1374,
         value: 'Fuel Oil #1',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 73.5038
+        emissionsOutputRate: 73.5038,
+        CO2: 73.25,
+        CH4: 3.0,
+        N2O: .6
     },
     {
         startingUnit: 'gal',
         heatCapacityValue: .1396,
         value: 'Fuel Oil #2',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 74.2138
+        emissionsOutputRate: 74.2138,
+        CO2: 73.96,
+        CH4: 3.0,
+        N2O: .6
     },
     {
         startingUnit: 'gal',
         heatCapacityValue: .1451,
         value: 'Fuel Oil #4',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 75.2938
+        emissionsOutputRate: 75.2938,
+        CO2: 75.04,
+        CH4: 3.0,
+        N2O: .6
     },
     {
         startingUnit: 'gal',
         heatCapacityValue: .1488,
         value: 'Fuel Oil #5',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 73.1838
+        emissionsOutputRate: 73.1838,
+        CO2: 72.93,
+        CH4: 3,
+        N2O: .6
     },
     {
         startingUnit: 'gal',
         heatCapacityValue: .1524,
         value: 'Fuel Oil #6 (high sulfur)',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 75.3538
+        emissionsOutputRate: 75.3538,
+        CO2: 75.10,
+        CH4: 3,
+        N2O: .6
     },
     {
         startingUnit: 'gal',
         heatCapacityValue: .149,
         value: 'Fuel Oil #6 (low sulfur)',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 75.3538
+        emissionsOutputRate: 75.3538,
+        CO2: 75.10,
+        CH4: 3,
+        N2O: .6
     },
     {
         startingUnit: 'gal',
         heatCapacityValue: .1387,
         value: 'Crude Oil',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 74.7938
+        emissionsOutputRate: 74.7938,
+        CO2: 74.54,
+        CH4: 3.0,
+        N2O: .6
     },
+    //CHECK VALUES: Gasoline = Motor Gasoline?
     {
         startingUnit: 'gal',
         heatCapacityValue: .12760,
         value: 'Gasoline',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 67.1338
+        emissionsOutputRate: 67.1338,
+        CO2: 70.22,
+        CH4: 3,
+        N2O: .6
     },
     {
         startingUnit: 'gal',
         heatCapacityValue: .1351,
         value: 'Kerosene',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 75.4538
+        emissionsOutputRate: 75.4538,
+        CO2: 75.2,
+        CH4: 3,
+        N2O: .6
     },
+    //CHECK VALUE: Gas Oil = Heavy Gas Oil?
     {
         startingUnit: 'gal',
         heatCapacityValue: .148,
         value: 'Gas Oil',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 75.1738
+        emissionsOutputRate: 75.1738,
+        CO2: 74.92,
+        CH4: 3,
+        N2O: .6
     },
+    //CHECK VALUE: Liquefied Natural Gas = Natural Gasoline? Wrong HHV
     {
         startingUnit: 'gal',
         heatCapacityValue: .086,
         value: 'Liquefied Natural Gas (LNG)',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 53.1148
+        emissionsOutputRate: 53.1148,
+        CO2: 66.88,
+        CH4: 3,
+        N2O: .6
     },
     {
         startingUnit: 'gal',
         heatCapacityValue: .128,
         value: 'Biodiesel (100%)',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 73.90028
+        emissionsOutputRate: 73.90028,
+        CO2: 73.84,
+        CH4: 1.1,
+        N2O: .11,
+        isBiofuel: true
     },
     {
         startingUnit: 'gal',
         heatCapacityValue: .084,
         value: 'Ethanol (100%)',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 68.50028
+        emissionsOutputRate: 68.50028,
+        CO2: 68.44,
+        CH4: 1.1,
+        N2O: .11,
+        isBiofuel: true
     }
 
 ]
@@ -250,70 +362,102 @@ export const SolidOptions: Array<FuelTypeOption> = [
         heatCapacityValue: 25.09,
         value: 'Coal (anthracite)',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 104.4418
+        emissionsOutputRate: 104.4418,
+        CO2: 103.69,
+        CH4: 11,
+        N2O: 1.6
     },
     {
         startingUnit: 'ton',
         heatCapacityValue: 24.93,
         value: 'Coal (bituminous)',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 94.0318
+        emissionsOutputRate: 94.0318,
+        CO2: 93.28,
+        CH4: 11,
+        N2O: 1.6
     },
     {
         startingUnit: 'ton',
         heatCapacityValue: 14.21,
         value: 'Coal (Lignite)',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 98.4718
+        emissionsOutputRate: 98.4718,
+        CO2: 97.72,
+        CH4: 11,
+        N2O: 1.6
     },
     {
         startingUnit: 'ton',
         heatCapacityValue: 17.25,
         value: 'Coal (subbituminous)',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 97.9218
+        emissionsOutputRate: 97.9218,
+        CO2: 97.17,
+        CH4: 11,
+        N2O: 1.6
     },
     {
         startingUnit: 'lb',
         heatCapacityValue: .013,
         value: 'Coke',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 104.4616
+        emissionsOutputRate: 104.4616,
+        CO2: 113.67,
+        CH4: 11,
+        N2O: 1.6
     },
     {
         startingUnit: 'lb',
         heatCapacityValue: .006,
         value: 'Peat',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 113.8916
+        emissionsOutputRate: 113.8916,
+        CO2: 111.84,
+        CH4: 32,
+        N2O: 4.2
     },
     {
         startingUnit: 'lb',
         heatCapacityValue: .008,
         value: 'Wood',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 95.0528
+        emissionsOutputRate: 95.0528,
+        CO2: 93.8,
+        CH4: 7.2,
+        N2O: 3.6
     },
+    //CHECK VALUES: No solid biomass in table
     {
         startingUnit: 'lb',
         heatCapacityValue: .004125,
         value: 'Biomass',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 120.2216
+        emissionsOutputRate: 120.2216,
+        CO2: 0,
+        CH4: 0,
+        N2O: 0
     },
+    //CHECK VALUES: No Black Liquor in table
     {
         startingUnit: 'lb',
         heatCapacityValue: .0065,
         value: 'Black Liquor',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 94.65266
+        emissionsOutputRate: 94.65266,
+        CO2: 0,
+        CH4: 0,
+        N2O: 0
     },
     {
         startingUnit: 'lb',
         heatCapacityValue: .016,
         value: 'Scrap Tires',
         siteToSourceMultiplier: 1,
-        emissionsOutputRate: 88.0216
+        emissionsOutputRate: 88.0216,
+        CO2: 85.97,
+        CH4: 32,
+        N2O: 4.2
     }
 ]
 
@@ -324,7 +468,10 @@ export const OtherEnergyOptions: Array<FuelTypeOption> = [
         value: 'Purchased Steam',
         siteToSourceMultiplier: 1.33,
         otherEnergyType: 'Steam',
-        emissionsOutputRate: 66.3985
+        emissionsOutputRate: 66.3985,
+        CO2: 0,
+        CH4: 0,
+        N2O: 0
     },
     {
         startingUnit: 'tonHr',
@@ -332,7 +479,10 @@ export const OtherEnergyOptions: Array<FuelTypeOption> = [
         value: 'Purchased Chilled Water (Absorption Chiller)',
         siteToSourceMultiplier: 1.25,
         otherEnergyType: 'Chilled Water',
-        emissionsOutputRate: 73.89
+        emissionsOutputRate: 73.89,
+        CO2: 0,
+        CH4: 0,
+        N2O: 0
     },
     {
         startingUnit: 'tonHr',
@@ -340,7 +490,10 @@ export const OtherEnergyOptions: Array<FuelTypeOption> = [
         value: 'Purchased Chilled Water (Electric-driven Compressor)',
         siteToSourceMultiplier: .24,
         otherEnergyType: 'Chilled Water',
-        emissionsOutputRate: 52.7
+        emissionsOutputRate: 52.7,
+        CO2: 0,
+        CH4: 0,
+        N2O: 0
     },
     {
         startingUnit: 'tonHr',
@@ -348,7 +501,10 @@ export const OtherEnergyOptions: Array<FuelTypeOption> = [
         value: 'Purchased Chilled Water (Engine-driven Compressor)',
         siteToSourceMultiplier: .83,
         otherEnergyType: 'Chilled Water',
-        emissionsOutputRate: 49.31
+        emissionsOutputRate: 49.31,
+        CO2: 0,
+        CH4: 0,
+        N2O: 0
     },
     {
         startingUnit: 'Therm',
@@ -356,7 +512,10 @@ export const OtherEnergyOptions: Array<FuelTypeOption> = [
         value: 'District Hot Water',
         siteToSourceMultiplier: undefined,
         otherEnergyType: 'Hot Water',
-        emissionsOutputRate: 66.3985
+        emissionsOutputRate: 66.3985,
+        CO2: 0,
+        CH4: 0,
+        N2O: 0
     },
     {
         startingUnit: 'SCF',
@@ -364,7 +523,10 @@ export const OtherEnergyOptions: Array<FuelTypeOption> = [
         value: 'Purchased Compressed Air',
         siteToSourceMultiplier: 3,
         otherEnergyType: 'Compressed Air',
-        emissionsOutputRate: undefined
+        emissionsOutputRate: undefined,
+        CO2: 0,
+        CH4: 0,
+        N2O: 0
     }
 ]
 
