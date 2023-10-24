c226b0b1eb69a50b3ce906e67f4c3ef83b5ef829
rmroot <rootrm@ornl.gov>
2023-10-24 12:11:28 -0500

    add missing delete emissions functionality

--- src/app/account/custom-database/regional-emissions-data/emissions-data-dashboard/emissions-data-dashboard.component.ts
+++ src/app/account/custom-database/regional-emissions-data/emissions-data-dashboard/emissions-data-dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Subscription, firstValueFrom } from 'rxjs';
+import { AccountdbService } from 'src/app/indexedDB/account-db.service';
 import { CustomEmissionsDbService } from 'src/app/indexedDB/custom-emissions-db.service';
-import { IdbAccount, IdbCustomEmissionsItem } from 'src/app/models/idb';
+import { FacilitydbService } from 'src/app/indexedDB/facility-db.service';
+import { IdbAccount, IdbCustomEmissionsItem, IdbFacility } from 'src/app/models/idb';
 
 @Component({
   selector: 'app-emissions-data-dashboard',
@@ -13,28 +15,68 @@ export class EmissionsDataDashboardComponent implements OnInit {
 
   customEmissionsItems: Array<IdbCustomEmissionsItem>;
   customEmissionsItemsSub: Subscription;
-  constructor(private customEmissionsDbService: CustomEmissionsDbService, private router: Router) { }
+  itemToDelete: IdbCustomEmissionsItem;
+  deleteItemInUse: boolean = false;
+  selectedAccount: IdbAccount;
+  selectedAccountSub: Subscription;
+  constructor(private customEmissionsDbService: CustomEmissionsDbService,
+    private router: Router,
+    private accountDbService: AccountdbService,
+    private facilityDbService: FacilitydbService) { }
 
   ngOnInit(): void {
     this.customEmissionsItemsSub = this.customEmissionsDbService.accountEmissionsItems.subscribe(val => {
       this.customEmissionsItems = val;
     });
+
+    this.selectedAccountSub = this.accountDbService.selectedAccount.subscribe(val => {
+      this.selectedAccount = val;
+    });
   }
 
   ngOnDestroy() {
     this.customEmissionsItemsSub.unsubscribe();
+    this.selectedAccountSub.unsubscribe();
   }
-
 
   addNewItem() {
     this.router.navigateByUrl('account/custom-data/emissions/add');
   }
 
   deleteItem(customEmissionsItem: IdbCustomEmissionsItem) {
-    //TODO...
+    this.itemToDelete = customEmissionsItem;
+    this.setDeleteItemInUse();
   }
 
   editItem(customEmissionsItem: IdbCustomEmissionsItem) {
     this.router.navigateByUrl('account/custom-data/emissions/edit/' + customEmissionsItem.guid);
+  }
+
+  setDeleteItemInUse() {
+    if (this.itemToDelete) {
+      this.deleteItemInUse = (this.itemToDelete.subregion == this.selectedAccount.eGridSubregion);
+      if (!this.deleteItemInUse) {
+        let facilities: Array<IdbFacility> = this.facilityDbService.accountFacilities.getValue();
+        facilities.forEach(facility => {
+          if (this.itemToDelete.subregion == facility.eGridSubregion) {
+            this.deleteItemInUse = true;
+          }
+        });
+      }
+    } else {
+      this.deleteItemInUse = false;
+    }
+  }
+
+  cancelDelete() {
+    this.itemToDelete = undefined;
+  }
+
+  async confirmDelete() {
+    await firstValueFrom(this.customEmissionsDbService.deleteWithObservable(this.itemToDelete.id));
+    let allEmissions: Array<IdbCustomEmissionsItem> = await firstValueFrom(this.customEmissionsDbService.getAll());
+    let accountEmissions: Array<IdbCustomEmissionsItem> = allEmissions.filter(fuel => { return fuel.accountId == this.selectedAccount.guid });
+    this.customEmissionsDbService.accountEmissionsItems.next(accountEmissions);
+    this.cancelDelete();
   }
 }
