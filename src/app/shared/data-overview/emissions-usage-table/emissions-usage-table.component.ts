3a7df504f6ca4bb60efda3119e164f34471e074d
rmroot <rootrm@ornl.gov>
2023-12-05 08:39:04 -0600

    update table and map for account emissions overview

--- src/app/shared/data-overview/emissions-usage-table/emissions-usage-table.component.ts
+++ src/app/shared/data-overview/emissions-usage-table/emissions-usage-table.component.ts
@@ -2,6 +2,8 @@ import { Component, Input } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { FacilityOverviewService } from 'src/app/facility/facility-overview/facility-overview.service';
 import { FacilityOverviewData, FacilityOverviewMeter } from 'src/app/calculations/dashboard-calculations/facilityOverviewClass';
+import { AccountOverviewData } from 'src/app/calculations/dashboard-calculations/accountOverviewClass';
+import { AccountOverviewService } from 'src/app/account/account-overview/account-overview.service';
 
 @Component({
   selector: 'app-emissions-usage-table',
@@ -10,18 +12,27 @@ import { FacilityOverviewData, FacilityOverviewMeter } from 'src/app/calculation
 })
 export class EmissionsUsageTableComponent {
   @Input()
-  facilityOverviewData: FacilityOverviewData
+  overviewData: FacilityOverviewData | AccountOverviewData
+  @Input()
+  inAccount: boolean;
 
   emissionsDisplay: 'market' | 'location';
   emissionsDisplaySub: Subscription;
 
   constructor(
-    private facilityOverviewService: FacilityOverviewService) { }
+    private facilityOverviewService: FacilityOverviewService,
+    private accountOverviewService: AccountOverviewService) { }
 
   ngOnInit(): void {
-    this.emissionsDisplaySub = this.facilityOverviewService.emissionsDisplay.subscribe(val => {
-      this.emissionsDisplay = val;
-    });
+    if(!this.inAccount){
+      this.emissionsDisplaySub = this.facilityOverviewService.emissionsDisplay.subscribe(val => {
+        this.emissionsDisplay = val;
+      });
+    }else {
+      this.emissionsDisplaySub = this.accountOverviewService.emissionsDisplay.subscribe(val => {
+        this.emissionsDisplay = val;
+      });
+    }
   }
 
   ngOnDestroy() {
