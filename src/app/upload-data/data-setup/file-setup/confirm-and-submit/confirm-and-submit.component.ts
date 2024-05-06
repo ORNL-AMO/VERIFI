b6f74c288def8d9922d5517b4029f264f421447d
rmroot <rootrm@ornl.gov>
2024-05-06 15:25:49 -0500

    properly skip meter data

--- src/app/upload-data/data-setup/file-setup/confirm-and-submit/confirm-and-submit.component.ts
+++ src/app/upload-data/data-setup/file-setup/confirm-and-submit/confirm-and-submit.component.ts
@@ -106,18 +106,18 @@ export class ConfirmAndSubmitComponent implements OnInit {
         }
 
         if (form.valid) {
-          if (meterData.id) {
-            let skipMeterData: boolean = false;
-            for (let x = 0; x < this.fileReference.skipExistingReadingsMeterIds.length; x++) {
-              if (this.fileReference.skipExistingReadingsMeterIds[x] == meterData.meterId) {
-                skipMeterData = true;
-              }
+          let skipMeterData: boolean = false;
+          for (let x = 0; x < this.fileReference.skipExistingReadingsMeterIds.length; x++) {
+            if (this.fileReference.skipExistingReadingsMeterIds[x] == meterData.meterId) {
+              skipMeterData = true;
             }
-            if (!skipMeterData) {
+          }
+          if (!skipMeterData) {
+            if (meterData.id) {
               await firstValueFrom(this.utilityMeterDataDbService.updateWithObservable(meterData));
+            } else {
+              await firstValueFrom(this.utilityMeterDataDbService.addWithObservable(meterData));
             }
-          } else {
-            await firstValueFrom(this.utilityMeterDataDbService.addWithObservable(meterData));
           }
         }
       }
