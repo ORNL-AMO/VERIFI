9ce3cbb3863400ce5493c50b7be5e5ac91d6fe63
rmroot <rootrm@ornl.gov>
2024-05-31 10:54:13 -0500

    updates to setup sidebar

--- src/app/setup-wizard/setup-wizard-sidebar/setup-wizard-sidebar.component.ts
+++ src/app/setup-wizard/setup-wizard-sidebar/setup-wizard-sidebar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-setup-wizard-sidebar',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './setup-wizard-sidebar.component.css'
 })
 export class SetupWizardSidebarComponent {
+
+  displaySidebar: boolean;
+  constructor(private router: Router){}
+
+  ngOnInit(){
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.setDisplaySidebar();
+      }
+    });
+    this.setDisplaySidebar();
+  }
+
+
+  setDisplaySidebar() {
+    this.displaySidebar = (this.router.url.includes('welcome') == false);
+  }
 
 }
