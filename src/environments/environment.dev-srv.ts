2bf7d0ec6fd42c348b6b07538bcbbd1a569b2dd6
rmroot <rootrm@ornl.gov>
2025-02-12 08:28:56 -0600

    fix environment vars with weather data api

--- src/environments/environment.dev-srv.ts
+++ src/environments/environment.dev-srv.ts
@@ -3,5 +3,6 @@ import packageInfo from '../../package.json';
 export const environment = {
   production: true,
   version: packageInfo.version,
-  measurUtilitiesApi: 'https://dev.weather.ornl.gov/'
+  measurUtilitiesApi: 'https://dev.weather.ornl.gov/',
+  weatherApi: 'https://lcd.ornl.gov/api'
 };
