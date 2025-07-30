cb5de0847c27fa220cb3beae757ac57e492b8c43
rmroot <rootrm@ornl.gov>
2025-07-29 19:37:05 -0500

    default todo list in side panel and fixes to help text logic

--- src/app/data-management/data-management-side-panel/data-management-help-panel/data-management-facility-help/data-management-facility-help.component.ts
+++ src/app/data-management/data-management-side-panel/data-management-help-panel/data-management-facility-help/data-management-facility-help.component.ts
@@ -1,6 +1,16 @@
 import { ChangeDetectorRef, Component } from '@angular/core';
 import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
+import { FacilitiesListComponent } from 'src/app/data-management/account-facilities/facilities-list/facilities-list.component';
+import { FacilityDataComponent } from 'src/app/data-management/account-facilities/facility-data/facility-data.component';
+import { FacilityMeterMonthlyDataComponent } from 'src/app/data-management/account-facilities/facility-data/facility-meters/facility-meter-monthly-data/facility-meter-monthly-data.component';
+import { FacilityMeterComponent } from 'src/app/data-management/account-facilities/facility-data/facility-meters/facility-meter/facility-meter.component';
+import { FacilityMetersTableComponent } from 'src/app/data-management/account-facilities/facility-data/facility-meters/facility-meters-table/facility-meters-table.component';
+import { FacilityPredictorComponent } from 'src/app/data-management/account-facilities/facility-data/facility-predictors/facility-predictor/facility-predictor.component';
+import { FacilityPredictorsTableComponent } from 'src/app/data-management/account-facilities/facility-data/facility-predictors/facility-predictors-table/facility-predictors-table.component';
+import { FacilitySetupComponent } from 'src/app/data-management/account-facilities/facility-data/facility-setup/facility-setup.component';
+import { MeterDataComponent } from 'src/app/shared/shared-meter-content/meter-data/meter-data.component';
+import { SetMeterGroupingComponent } from 'src/app/shared/shared-meter-content/set-meter-grouping/set-meter-grouping.component';
 
 @Component({
   selector: 'app-data-management-facility-help',
@@ -42,36 +52,36 @@ export class DataManagementFacilityHelpComponent {
     if (!childRoute) {
       return;
     }
-    let currentComponent = childRoute.component.name;
-    if (currentComponent === 'FacilitiesListComponent') {
+    let currentComponent = childRoute.snapshot.routeConfig?.component;
+    if (currentComponent === FacilitiesListComponent) {
       this.helpContext = 'facilities-list'
-    } else if (currentComponent === 'FacilityDataComponent') {
+    } else if (currentComponent === FacilityDataComponent) {
       if (childRoute.firstChild) {
         if (childRoute.firstChild.component) {
-          let subComponent = childRoute.firstChild.component.name;
-          if (subComponent === 'FacilitySetupComponent') {
+          let subComponent = childRoute.firstChild.routeConfig?.component;
+          if (subComponent === FacilitySetupComponent) {
             this.helpContext = 'facility-setup';
-          } else if (subComponent === 'FacilityMeterComponent') {
+          } else if (subComponent === FacilityMeterComponent) {
             this.helpContext = 'facility-setup';
-          } else if (subComponent === 'FacilityMeterMonthlyDataComponent') {
+          } else if (subComponent === FacilityMeterMonthlyDataComponent) {
             this.helpContext = 'facility-setup';
-          } else if (subComponent === 'FacilityMetersTableComponent') {
+          } else if (subComponent === FacilityMetersTableComponent) {
             this.helpContext = 'facility-meters'
-          } else if (subComponent === 'SetMeterGroupingComponent') {
+          } else if (subComponent === SetMeterGroupingComponent) {
             this.helpContext = 'facility-meter-grouping'
-          } else if (subComponent === 'FacilityPredictorsTableComponent') {
+          } else if (subComponent === FacilityPredictorsTableComponent) {
             this.helpContext = 'facility-predictors';
           }
         } else {
-          if (childRoute.firstChild.firstChild && childRoute.firstChild.firstChild.component) {
-            let subComponent = childRoute.firstChild.firstChild.component.name;
-            if (subComponent === 'MeterDataComponent') {
+          if (childRoute.firstChild.firstChild && childRoute.firstChild.firstChild.routeConfig?.component) {
+            let subComponent = childRoute.firstChild.firstChild.routeConfig?.component;
+            if (subComponent === MeterDataComponent) {
               this.helpContext = 'facility-meter-data';
-            } else if (subComponent === 'FacilityMeterComponent') {
+            } else if (subComponent === FacilityMeterComponent) {
               this.helpContext = 'facility-meter-settings';
-            } else if (subComponent === 'FacilityMeterMonthlyDataComponent') {
+            } else if (subComponent === FacilityMeterMonthlyDataComponent) {
               this.helpContext = 'facility-meter-monthly-data';
-            } else if (subComponent === 'FacilityPredictorComponent') {
+            } else if (subComponent === FacilityPredictorComponent) {
               this.helpContext = 'facility-predictors';
             }
           } else {
