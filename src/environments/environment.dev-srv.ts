a8371edead4154a661e7cb52f7f75408f70a1669
rmroot <rootrm@ornl.gov>
2025-08-13 08:44:41 -0500

    update example and survey server url

--- src/environments/environment.dev-srv.ts
+++ src/environments/environment.dev-srv.ts
@@ -3,6 +3,6 @@ import packageInfo from '../../package.json';
 export const environment = {
   production: true,
   version: packageInfo.version,
-  measurUtilitiesApi: 'https://dev.weather.ornl.gov/',
+  measurUtilitiesApi: 'https://dev.ir-utilities.ornl.gov/',
   weatherApi: 'https://lcd.ornl.gov/api'
 };
