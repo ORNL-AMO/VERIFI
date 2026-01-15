3bb865cd834e5e12ac7df92d409f25f9ce1b7409
Mark Root <rootrm@ornl.gov>
2026-01-15 09:16:22 -0600

    feat: add collapsing tabs to account and facility

--- src/app/data-evaluation/facility/facility-banner/facility-banner.component.ts
+++ src/app/data-evaluation/facility/facility-banner/facility-banner.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { FacilitydbService } from 'src/app/indexedDB/facility-db.service';
 import { UtilityMeterDatadbService } from 'src/app/indexedDB/utilityMeterData-db.service';
@@ -6,20 +6,26 @@ import { IdbFacility } from 'src/app/models/idbModels/facility';
 import { IdbUtilityMeterData } from 'src/app/models/idbModels/utilityMeterData';
 
 @Component({
-    selector: 'app-facility-banner',
-    templateUrl: './facility-banner.component.html',
-    styleUrls: ['./facility-banner.component.css'],
-    standalone: false
+  selector: 'app-facility-banner',
+  templateUrl: './facility-banner.component.html',
+  styleUrls: ['./facility-banner.component.css'],
+  standalone: false
 })
 export class FacilityBannerComponent implements OnInit {
+
+  @ViewChild('navTabs') navTabs: ElementRef;
 
   selectedFacility: IdbFacility;
   selectedFacilitySub: Subscription;
 
   facilityMeterData: Array<IdbUtilityMeterData>;
   facilityMeterDataSub: Subscription;
+
+  hideTabText: boolean = false;
+  hideAllText: boolean = false;
   constructor(private facilityDbService: FacilitydbService,
-     private utilityMeterDataDbService: UtilityMeterDatadbService) { }
+    private utilityMeterDataDbService: UtilityMeterDatadbService,
+    private cd: ChangeDetectorRef) { }
 
   ngOnInit(): void {
     this.selectedFacilitySub = this.facilityDbService.selectedFacility.subscribe(val => {
@@ -34,5 +40,21 @@ export class FacilityBannerComponent implements OnInit {
   ngOnDestroy() {
     this.selectedFacilitySub.unsubscribe();
     this.facilityMeterDataSub.unsubscribe();
+  }
+
+  ngAfterViewInit() {
+    this.setHideTabText();
+    this.cd.detectChanges();
+  }
+
+  setHideTabText() {
+    this.hideTabText = this.navTabs.nativeElement.offsetWidth < 750;
+    this.hideAllText = this.navTabs.nativeElement.offsetWidth < 550;
+  }
+
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.setHideTabText();
   }
 }
