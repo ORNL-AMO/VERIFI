b740339f09043caff5ef16691e19469553517734
Mark Root <rootrm@ornl.gov>
2026-08-12 12:16:11 -0500

    fixup(reports): migrate data overview report setup to signals to fix report year bug

--- src/app/data-evaluation/account/account-reports/account-report-setup/data-overview-setup/data-overview-setup.component.ts
+++ src/app/data-evaluation/account/account-reports/account-report-setup/data-overview-setup/data-overview-setup.component.ts
@@ -1,13 +1,9 @@
 import { AccountWorkspaceService } from 'src/app/account-workspace/account-workspace.service';
-import { toObservable } from '@angular/core/rxjs-interop';
 import { AccountWorkspaceStore } from 'src/app/account-workspace/account-workspace.store';
 import { WorkspaceCommandBoundary } from 'src/app/account-workspace/workspace-command-boundary.service';
 import { ReportCommandHandler } from 'src/app/account-workspace/handlers/report-command-handler.service';
-import { Component, inject, Injector } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { IdbAccount } from 'src/app/models/idbModels/account';
+import { Component, computed, effect, inject } from '@angular/core';
 import { IdbAccountReport } from 'src/app/models/idbModels/accountReport';
-import { IdbUtilityMeter } from 'src/app/models/idbModels/utilityMeter';
 import { DataOverviewReportSetup } from 'src/app/models/overview-report';
 
 @Component({
@@ -22,56 +18,44 @@ export class DataOverviewSetupComponent {
   private readonly commandBoundary = inject(WorkspaceCommandBoundary);
   private readonly reportHandler = inject(ReportCommandHandler);
 
+  readonly account = this.accountWorkspaceStore.account;
+
   // overviewForm: FormGroup;
-  account: IdbAccount;
-  selectedReportSub: Subscription;
-  isFormChange: boolean = false;
   reportSetup: DataOverviewReportSetup;
-  showWater: boolean;
-  constructor(
-    private injector: Injector
-  ) {
-  }
 
+  readonly showWater = computed(() => {
+    const meters = this.accountWorkspaceStore.meters();
+    return meters.some(meter => meter.source === 'Water Intake' || meter.source === 'Water Discharge');
+  });
 
-  ngOnInit() {
-    this.account = this.accountWorkspaceStore.account();
-    this.selectedReportSub = toObservable(this.accountWorkspaceStore.selectedAccountReport, { injector: this.injector }).subscribe(val => {
+  private isFormChange = false;
+
+  constructor() {
+    effect(() => {
+      const report = this.accountWorkspaceStore.selectedAccountReport();
       if (!this.isFormChange) {
-        this.reportSetup = val.dataOverviewReportSetup;
-        this.setShowWater();
+        this.reportSetup = report.dataOverviewReportSetup;
+        if (!this.showWater() && this.reportSetup?.includeWaterSection) {
+          this.reportSetup.includeWaterSection = false;
+          this.save();
+        }
       } else {
         this.isFormChange = false;
       }
     });
   }
 
-  ngOnDestroy() {
-    this.selectedReportSub.unsubscribe();
-  }
-
   async save() {
     this.isFormChange = true;
-    let selectedReport: IdbAccountReport = this.accountWorkspaceStore.selectedAccountReport()
+    let selectedReport: IdbAccountReport = this.accountWorkspaceStore.selectedAccountReport();
     // selectedReport.dataOverviewReportSetup = this.accountReportsService.updateDataOverviewReportFromForm(selectedReport.dataOverviewReportSetup, this.overviewForm);
     selectedReport.dataOverviewReportSetup = this.reportSetup;
-    const activeAccountGuid = this.accountWorkspaceStore.account()?.guid;
+    const activeAccountGuid = this.account()?.guid;
     await this.commandBoundary.execute(
-      { entityKind: 'accountReport', changeKind: 'update', entityGuid: selectedReport.guid, label: 'Save Report' ,
+      { entityKind: 'accountReport', changeKind: 'update', entityGuid: selectedReport.guid, label: 'Save Report',
         publication: { mode: 'patch', buildPatch: value => ({ collections: [{ collection: 'accountReports', upsert: [value] }] }) }},
       () => this.reportHandler.updateAccountReport(selectedReport, activeAccountGuid)
     );
     this.accountWorkspaceService.selectAccountReport(({ ...selectedReport })?.guid);
   }
-
-  setShowWater() {
-    let accountMeters: Array<IdbUtilityMeter> = [...this.accountWorkspaceStore.meters()];
-    let waterMeter: IdbUtilityMeter = accountMeters.find(meter => { return meter.source == 'Water Intake' || meter.source == 'Water Discharge' });
-    this.showWater = waterMeter != undefined;
-    if (!this.showWater && this.reportSetup?.includeWaterSection) {
-      this.reportSetup.includeWaterSection = false;
-      this.save();
-    }
-  }
-
 }
--- src/app/data-evaluation/facility/facility-reports/facility-report-setup/facility-overview-report-setup/facility-overview-report-setup.component.ts
+++ src/app/data-evaluation/facility/facility-reports/facility-report-setup/facility-overview-report-setup/facility-overview-report-setup.component.ts
@@ -1,11 +1,8 @@
 import { AccountWorkspaceService } from 'src/app/account-workspace/account-workspace.service';
-import { toObservable } from '@angular/core/rxjs-interop';
 import { AccountWorkspaceStore } from 'src/app/account-workspace/account-workspace.store';
 import { WorkspaceCommandBoundary } from 'src/app/account-workspace/workspace-command-boundary.service';
 import { ReportCommandHandler } from 'src/app/account-workspace/handlers/report-command-handler.service';
-import { Component, inject, Injector } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { IdbAccount } from 'src/app/models/idbModels/account';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { DataOverviewFacilityReportSettings, IdbFacilityReport } from 'src/app/models/idbModels/facilityReport';
 import { Month, Months } from 'src/app/shared/form-data/months';
 import { CalanderizationService } from 'src/app/shared/helper-services/calanderization.service';
@@ -21,78 +18,59 @@ export class FacilityOverviewReportSetupComponent {
   private readonly accountWorkspaceStore = inject(AccountWorkspaceStore);
   private readonly commandBoundary = inject(WorkspaceCommandBoundary);
   private readonly reportHandler = inject(ReportCommandHandler);
+  private readonly calanderizationService = inject(CalanderizationService);
+
+  readonly account = this.accountWorkspaceStore.account;
 
   facilityReport: IdbFacilityReport;
   reportSettings: DataOverviewFacilityReportSettings;
-  facilityReportSub: Subscription;
-  isFormChange: boolean = false;
-  reportYears: Array<number>;
-  baselineYears: Array<number>;
-  months: Array<Month> = Months;
-  account: IdbAccount;
-  accountSub: Subscription;
-  invalidDateRange: boolean = false;
-  constructor(
-    private calanderizationService: CalanderizationService,
-    private injector: Injector
+  readonly months: Array<Month> = Months;
 
-  ) {
+  private readonly _facilityReport = signal<IdbFacilityReport>(undefined);
 
-  }
+  readonly reportYears = computed(() => {
+    const report = this._facilityReport();
+    if (!report) return [];
+    //TODO: baseline years less than report year selection
+    //TODO: report years greater than baseline year selection
+    //TODO: get options by water/energy
+    return this.calanderizationService.getYearOptions('all', true, report.facilityId);
+  });
+  readonly baselineYears = this.reportYears;
 
-  ngOnInit() {
-    this.accountSub = toObservable(this.accountWorkspaceStore.account, { injector: this.injector }).subscribe(account => {
-      this.account = account;
-    });
-    this.facilityReportSub = toObservable(this.accountWorkspaceStore.selectedFacilityReport, { injector: this.injector }).subscribe(report => {
-      if (this.isFormChange == false) {
+  readonly invalidDateRange = computed(() => {
+    const settings = this.reportSettings;
+    if (!settings?.startYear || !settings?.endYear) return false;
+    if (settings.startYear !== settings.endYear) return settings.startYear > settings.endYear;
+    return settings.startMonth > settings.endMonth;
+  });
+
+  private isFormChange = false;
+
+  constructor() {
+    effect(() => {
+      const report = this.accountWorkspaceStore.selectedFacilityReport();
+      if (!this.isFormChange) {
         this.facilityReport = report;
-        this.reportSettings = this.facilityReport.dataOverviewReportSettings;
+        this.reportSettings = report?.dataOverviewReportSettings;
+        this._facilityReport.set(report);
       } else {
         this.isFormChange = false;
       }
-      this.setInvalidDateRange();
     });
-    this.setYearOptions();
-  }
-
-  ngOnDestroy() {
-    this.facilityReportSub.unsubscribe();
-    this.accountSub.unsubscribe();
   }
 
   async save() {
     this.isFormChange = true;
-    let facilityReport: IdbFacilityReport = this.accountWorkspaceStore.selectedFacilityReport();
     this.facilityReport.dataOverviewReportSettings = this.reportSettings;
-    const activeAccountGuid = this.accountWorkspaceStore.account()?.guid;
+    const activeAccountGuid = this.account()?.guid;
     const { value: updatedReport } = await this.commandBoundary.execute(
-      { entityKind: 'facilityReport', changeKind: 'update', entityGuid: this.facilityReport.guid, label: 'Save Report' ,
+      { entityKind: 'facilityReport', changeKind: 'update', entityGuid: this.facilityReport.guid, label: 'Save Report',
         publication: { mode: 'patch', buildPatch: value => ({ collections: [{ collection: 'facilityReports', upsert: [value] }] }) }},
       () => this.reportHandler.updateFacilityReport(this.facilityReport, activeAccountGuid)
     );
     this.facilityReport = updatedReport;
-    this.accountWorkspaceService.selectFacilityReport(this.facilityReport?.guid);
-  }
-
-  setYearOptions() {
-    //TODO: baseline years less than report year selection
-    //TODO: report years greater than baseline year selection
-    //TODO: get options by water/energy
-    let yearOptions: Array<number> = this.calanderizationService.getYearOptions('all', true, this.facilityReport.facilityId);
-    this.reportYears = yearOptions;
-    this.baselineYears = yearOptions;
-  }
-
-  setInvalidDateRange(){
-    if(this.reportSettings.startYear && this.reportSettings.endYear){
-      if(this.reportSettings.startYear !== this.reportSettings.endYear){
-        this.invalidDateRange = this.reportSettings.startYear > this.reportSettings.endYear;
-      } else {
-        this.invalidDateRange = this.reportSettings.startMonth > this.reportSettings.endMonth;
-      }
-    } else {
-      this.invalidDateRange = false;
-    }
+    this._facilityReport.set(updatedReport);
+    this.accountWorkspaceService.selectFacilityReport(updatedReport?.guid);
   }
 }
