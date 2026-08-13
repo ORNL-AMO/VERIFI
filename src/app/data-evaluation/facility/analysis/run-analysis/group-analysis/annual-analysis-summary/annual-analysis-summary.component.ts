65e698c7504610c7b9850fba0d43279b4be054c1
Mark Root <rootrm@ornl.gov>
2026-08-13 11:50:54 -0500

    fixup(analysis): migrate group analysis results pages to use signals

--- src/app/data-evaluation/facility/analysis/run-analysis/group-analysis/annual-analysis-summary/annual-analysis-summary.component.ts
+++ src/app/data-evaluation/facility/analysis/run-analysis/group-analysis/annual-analysis-summary/annual-analysis-summary.component.ts
@@ -1,6 +1,6 @@
-import { toObservable } from '@angular/core/rxjs-interop';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { AccountWorkspaceStore } from 'src/app/account-workspace/account-workspace.store';
-import { Component, OnInit, inject, Injector } from '@angular/core';
+import { Component, DestroyRef, Signal, computed, effect, inject, signal } from '@angular/core';
 import { AnalysisService } from 'src/app/data-evaluation/facility/analysis/analysis.service';
 import { AnalysisGroup, AnnualAnalysisSummary } from 'src/app/models/analysis';
 import { CalanderizedMeter } from 'src/app/models/calanderization';
@@ -8,13 +8,7 @@ import { AnnualGroupAnalysisSummaryClass } from 'src/app/calculations/analysis-c
 import { getCalanderizedMeterData } from 'src/app/calculations/calanderization/calanderizeMeters';
 import { getNeededUnits } from 'src/app/calculations/shared-calculations/calanderizationFunctions';
 import { IdbFacility } from 'src/app/models/idbModels/facility';
-import { IdbUtilityMeter } from 'src/app/models/idbModels/utilityMeter';
-import { IdbUtilityMeterData } from 'src/app/models/idbModels/utilityMeterData';
-import { IdbPredictorData } from 'src/app/models/idbModels/predictorData';
-import { IdbPredictor } from 'src/app/models/idbModels/predictor';
 import { IdbAnalysisItem } from 'src/app/models/idbModels/analysisItem';
-import { IdbAccount } from 'src/app/models/idbModels/account';
-import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-annual-analysis-summary',
@@ -22,58 +16,56 @@ import { Subscription } from 'rxjs';
     styleUrls: ['./annual-analysis-summary.component.css'],
     standalone: false
 })
-export class AnnualAnalysisSummaryComponent implements OnInit {
+export class AnnualAnalysisSummaryComponent {
   private readonly accountWorkspaceStore = inject(AccountWorkspaceStore);
+  private readonly analysisService = inject(AnalysisService);
+  private readonly destroyRef = inject(DestroyRef);
 
-  analysisItem: IdbAnalysisItem;
-  group: AnalysisGroup;
-  facility: IdbFacility;
-  annualAnalysisSummary: Array<AnnualAnalysisSummary>;
-  worker: Worker;
-  calculating: boolean | 'error';
-  analysisDisplay: 'table' | 'graph';
-  key: string;
-  facilitySub: Subscription;
+  readonly analysisItem: Signal<IdbAnalysisItem> = this.accountWorkspaceStore.selectedFacilityAnalysis;
+  readonly facility: Signal<IdbFacility> = this.accountWorkspaceStore.selectedFacility;
+  readonly selectedGroup: Signal<AnalysisGroup> = toSignal(this.analysisService.selectedGroup);
 
-  constructor(
-    private analysisService: AnalysisService,
-    private injector: Injector
-  ) {
-  }
+  readonly key: Signal<string> = computed(() => 'annual-' + this.facility()?.id);
+  readonly analysisDisplay = signal<'table' | 'graph'>('table');
 
-  ngOnInit(): void {
-    this.analysisItem = this.accountWorkspaceStore.selectedFacilityAnalysis();
-    let accountAnalysisItems: Array<IdbAnalysisItem> = [...this.accountWorkspaceStore.facilityAnalyses()];
-    this.group = this.analysisService.selectedGroup.getValue();
+  readonly annualAnalysisSummary = signal<Array<AnnualAnalysisSummary>>(undefined);
+  readonly calculating = signal<boolean | 'error'>(false);
 
-    this.facilitySub = toObservable(this.accountWorkspaceStore.selectedFacility, { injector: this.injector }).subscribe(val => {
-      this.facility = val;
-      this.key = 'annual-' + this.facility?.id;
-      this.analysisDisplay = this.analysisService.getDisplaySubject(this.key, 'table').getValue();
+  private worker: Worker;
+
+  constructor() {
+    effect(() => {
+      const key = this.key();
+      this.analysisDisplay.set(this.analysisService.getDisplaySubject(key, 'table').getValue());
     });
 
-    let facilityMeters: Array<IdbUtilityMeter> = [...this.accountWorkspaceStore.facilityMeters()];
-    let facilityMeterData: Array<IdbUtilityMeterData> = [...this.accountWorkspaceStore.facilityMeterData()];
-    let accountPredictorEntries: Array<IdbPredictorData> = [...this.accountWorkspaceStore.predictorData()];
-    let accountPredictors: Array<IdbPredictor> = [...this.accountWorkspaceStore.predictors()];
-    let account: IdbAccount = this.accountWorkspaceStore.account();
+    const analysisItem = this.analysisItem();
+    const facility = this.facility();
+    const group = this.selectedGroup();
+    const facilityMeters = [...this.accountWorkspaceStore.facilityMeters()];
+    const facilityMeterData = [...this.accountWorkspaceStore.facilityMeterData()];
+    const accountPredictorEntries = [...this.accountWorkspaceStore.predictorData()];
+    const accountPredictors = [...this.accountWorkspaceStore.predictors()];
+    const accountAnalysisItems = [...this.accountWorkspaceStore.facilityAnalyses()];
+    const account = this.accountWorkspaceStore.account();
+
     if (typeof Worker !== 'undefined') {
       this.worker = new Worker(new URL('../../../../../../web-workers/annual-group-analysis.worker', import.meta.url));
       this.worker.onmessage = ({ data }) => {
         if (!data.error) {
-          this.annualAnalysisSummary = data.annualAnalysisSummaries;
-          this.calculating = false;
+          this.annualAnalysisSummary.set(data.annualAnalysisSummaries);
+          this.calculating.set(false);
         } else {
-          this.annualAnalysisSummary = undefined;
-          this.calculating = 'error';
+          this.annualAnalysisSummary.set(undefined);
+          this.calculating.set('error');
         }
         this.worker.terminate();
       };
-      this.calculating = true;
+      this.calculating.set(true);
       this.worker.postMessage({
-        selectedGroup: this.group,
-        analysisItem: this.analysisItem,
-        facility: this.facility,
+        selectedGroup: group,
+        analysisItem: analysisItem,
+        facility: facility,
         meters: facilityMeters,
         meterData: facilityMeterData,
         accountPredictorEntries: accountPredictorEntries,
@@ -83,22 +75,21 @@ export class AnnualAnalysisSummaryComponent implements OnInit {
       });
     } else {
       // Web Workers are not supported in this environment.
-      let calanderizedMeters: Array<CalanderizedMeter> = getCalanderizedMeterData(facilityMeters, facilityMeterData, this.facility, false, { energyIsSource: this.analysisItem.energyIsSource, neededUnits: getNeededUnits(this.analysisItem) }, [], [], [this.facility], account.assessmentReportVersion, []);
-      let annualAnalysisSummaryClass: AnnualGroupAnalysisSummaryClass = new AnnualGroupAnalysisSummaryClass(this.group, this.analysisItem, this.facility, calanderizedMeters, accountPredictorEntries, undefined, accountPredictors, accountAnalysisItems);
-      this.annualAnalysisSummary = annualAnalysisSummaryClass.getAnnualAnalysisSummaries();
-      this.calculating = false;
+      const calanderizedMeters: Array<CalanderizedMeter> = getCalanderizedMeterData(facilityMeters, facilityMeterData, facility, false, { energyIsSource: analysisItem.energyIsSource, neededUnits: getNeededUnits(analysisItem) }, [], [], [facility], account.assessmentReportVersion, []);
+      const annualAnalysisSummaryClass = new AnnualGroupAnalysisSummaryClass(group, analysisItem, facility, calanderizedMeters, accountPredictorEntries, undefined, accountPredictors, accountAnalysisItems);
+      this.annualAnalysisSummary.set(annualAnalysisSummaryClass.getAnnualAnalysisSummaries());
+      this.calculating.set(false);
     }
-  }
 
-  ngOnDestroy() {
-    if (this.worker) {
-      this.worker.terminate();
-    }
-    this.facilitySub.unsubscribe();
+    this.destroyRef.onDestroy(() => {
+      if (this.worker) {
+        this.worker.terminate();
+      }
+    });
   }
 
   setDataDisplay(display: 'table' | 'graph') {
-    this.analysisDisplay = display;
-    this.analysisService.getDisplaySubject(this.key, 'table').next(this.analysisDisplay);
+    this.analysisDisplay.set(display);
+    this.analysisService.getDisplaySubject(this.key(), 'table').next(display);
   }
 }
